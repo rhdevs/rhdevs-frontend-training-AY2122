92570d8b3a665a3b665afb3021283aa3147da0bf
hui444 <e0425552@u.nus.edu>
2021-09-16 00:20:41 +0800

    Update landing page to show group cards

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import styled from 'styled-components'
+import { PATHS } from '../../App'
 import frontend_webdev from '../../assets/frontend_webdev.svg'
 
 // Do not edit the other parts of the code!
@@ -26,11 +28,45 @@ const InformationCardSection = styled.div`
   justify-content: space-evenly;
 `
 
-const IndivMemberContainer = styled.div`
-  border: 1px dashed grey;
+// Create a styled div for your card here
+const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #f6f6f6;
+  border-radius: 10px;
+  overflow: hidden;
+  filter: drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.25));
+  cursor: pointer;
+  transition: transform 0.5s;
+  justify-content: center;
+  align-items: center;
+
+  :hover,
+  :focus {
+    transform: scale3d(1.05, 1.05, 1.1);
+  }
 `
 
-// Create a styled div for your card here
+const StyledGroupNumber = styled.text`
+  font-size: calc(18px + 2vw);
+`
+
+const StyledComponentName = styled.text`
+  font-size: calc(16px + 0.5vw);
+`
+
+const StyledMemberNameGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const BoldText = styled.text`
+  font-weight: 500;
+`
+
+const StyledNamesText = styled.text`
+  padding-left: 5px;
+`
 
 const LandingPage = () => {
   useEffect(() => {
@@ -38,13 +74,73 @@ const LandingPage = () => {
     window.scrollTo(0, 0)
   }, [])
 
+  const history = useHistory()
   return (
     <>
       <FEImg src={frontend_webdev} alt="frontend web development" />
       <MembersSection>
-        <MembersSectionHeader>You&#39;re in good company</MembersSectionHeader>
+        <MembersSectionHeader>Groups!</MembersSectionHeader>
         <InformationCardSection>
-          <IndivMemberContainer>{/* Insert code here */}</IndivMemberContainer>
+          <StyledCard>
+            <StyledGroupNumber>Group 1</StyledGroupNumber>
+            <StyledComponentName>component name</StyledComponentName>
+            <StyledMemberNameGroup>
+              <BoldText>Devs:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+            <StyledMemberNameGroup>
+              <BoldText>Designers:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+          </StyledCard>
+          <StyledCard>
+            <StyledGroupNumber>Group 2</StyledGroupNumber>
+            <StyledComponentName>component name</StyledComponentName>
+            <StyledMemberNameGroup>
+              <BoldText>Devs:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+            <StyledMemberNameGroup>
+              <BoldText>Designers:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+          </StyledCard>
+          <StyledCard>
+            <StyledGroupNumber>Group 3</StyledGroupNumber>
+            <StyledComponentName>component name</StyledComponentName>
+            <StyledMemberNameGroup>
+              <BoldText>Devs:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+            <StyledMemberNameGroup>
+              <BoldText>Designers:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+          </StyledCard>
+          <StyledCard>
+            <StyledGroupNumber>Group 4</StyledGroupNumber>
+            <StyledComponentName>component name</StyledComponentName>
+            <StyledMemberNameGroup>
+              <BoldText>Devs:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+            <StyledMemberNameGroup>
+              <BoldText>Designers:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+          </StyledCard>
+          <StyledCard>
+            <StyledGroupNumber>Group 5</StyledGroupNumber>
+            <StyledComponentName>component name</StyledComponentName>
+            <StyledMemberNameGroup>
+              <BoldText>Devs:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+            <StyledMemberNameGroup>
+              <BoldText>Designers:</BoldText>
+              <StyledNamesText>name1, name2, name4</StyledNamesText>
+            </StyledMemberNameGroup>
+          </StyledCard>
         </InformationCardSection>
       </MembersSection>
     </>
