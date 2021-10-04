f687c527d7ef38ff1249e97d3c0f1a6f484f78e9
Ang Ki Yong <angkiyong@gmail.com>
2021-10-04 22:06:49 +0800

    skeleton for component

---
+++ src/components/FriendCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  
+`
+
+const FriendCard = () => {
+  return (
+    <StyledCard>
+    </StyledCard>
+  )
+}
+
+export default FriendCard
