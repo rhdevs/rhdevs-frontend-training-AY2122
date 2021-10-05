c8021299460785e7ac28766029037d298d1c7f1b
Marcus Ong <marcus.ong.qy@gmail.com>
2021-10-05 18:01:30 +0800

    formatted group page nicely

--- src/pages/Groups/Group4/index.tsx
+++ src/pages/Groups/Group4/index.tsx
@@ -10,6 +10,7 @@ import OldPasswordPage from '../../../assets/group4/Forget Password.svg'
 
 const MainContainer = styled.div`
   margin: 1rem 2rem;
+  height: 100%;
 `
 
 const PlaceholderBox = styled.div`
@@ -27,11 +28,26 @@ const ComponentsContainer = styled.div`
   grid-template-columns: 1fr;
   grid-template-rows: 1fr 1fr;
   grid-row-gap: 3vw;
-  height: 50vh;
+  height: 100%;
 `
 
 const StyledText = styled.p`
   font-size: 18px;
+`
+const OldImages = styled.p`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const NewPages = styled.p`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `
 
 // Create something similiar for your group's page
@@ -43,17 +59,29 @@ const Group4 = () => {
       <ComponentsContainer>
         <PlaceholderBox>
           <h2>Old Images Preview</h2>
-          <div id="old-images">
-            <Image src={OldLoginPage} alt={'Old Login Page'} width="340px" height="406px" />
-            <Image src={OldPasswordPage} alt={'Old Password Page'} width="340px" height="406px" />
-          </div>
+          <OldImages>
+            <Image
+              src={OldLoginPage}
+              alt={'Old Login Page'}
+              style={{ alignItems: 'center' }}
+              width="414px"
+              height="896px"
+            />
+            <Image
+              src={OldPasswordPage}
+              alt={'Old Password Page'}
+              style={{ alignItems: 'center' }}
+              width="414px"
+              height="896px"
+            />
+          </OldImages>
         </PlaceholderBox>
         <PlaceholderBox>
           <h2>New Pages</h2>
-          <div id="new pages">
+          <NewPages>
             <LoginPage />
             <ForgotPasswordPage />
-          </div>
+          </NewPages>
         </PlaceholderBox>
       </ComponentsContainer>
     </MainContainer>
