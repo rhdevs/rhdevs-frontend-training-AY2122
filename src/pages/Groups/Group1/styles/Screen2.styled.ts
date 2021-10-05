9004c6cc6952eb2081f2ba07c5537d192d32a1e5
chun <lamchunyu00@gmail.com>
2021-10-05 21:28:56 +0800

    Added UI Improvements and followed Figma styles for implementation

--- src/pages/Groups/Group1/styles/Screen2.styled.ts
+++ src/pages/Groups/Group1/styles/Screen2.styled.ts
@@ -2,32 +2,67 @@ import styled from 'styled-components'
 
 export const Bookings_container = styled.div<{ fontSize?: string }>`
   margin: 0;
+  height: 100%;
+  width: 100%;
   color: black;
-  border-style: solid;
+  padding: 7vh 0px;
+  //border-style: solid;
   white-space: nowrap;
   ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
 `
-
-export const Individual_bookings_1 = styled.div<{ fontSize?: string }>`
-  margin: 0;
-  color: black;
-  background-color: #468751;
-  white-space: nowrap;
-  ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
+export const TopNav = styled.div`
+  width: 100%;
+  height: 7vh;
+  padding: 1em;
+  font-size: 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  background: #cecece;
+`
+export const BotNav = styled.div`
+  width: 100%;
+  height: 7vh;
+  padding: 1em;
+  font-size: 28px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  background: #cecece;
+  z-index: 800;
+  overflow: hidden;
+  bottom: 0;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `
 
-export const Individual_bookings_2 = styled.div<{ fontSize?: string }>`
+export const Header = styled.h1`
+  font-size: 23px;
   margin: 0;
-  color: black;
-  background-color: white;
-  white-space: nowrap;
-  ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
 `
 
-export const Individual_bookings_3 = styled.div<{ fontSize?: string }>`
-  margin: 0;
+export const IndividualBookings = styled.div<{ background?: string }>`
+  margin: 15px 2%;
+  border-radius: 5px;
+  border: solid 1px;
+  padding: 30px;
   color: black;
-  background-color: green;
+  ${(props) => (props.background && props.background === 'green' ? `background: #468751;` : `background: white`)}
   white-space: nowrap;
-  ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
+  min-height: 100px;
+`
+export const EventText = styled.h3<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  font-weight: 200;
+`
+
+export const LocationText = styled.p<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  font-weight: 500;
+`
+
+export const StyledButton = styled.img`
+  width: 28px;
+  max-height: 28px;
 `
