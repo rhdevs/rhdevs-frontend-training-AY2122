0fc721718a80816bf4808aff6367cf6cc0ca249f
chun <lamchunyu00@gmail.com>
2021-10-05 11:15:18 +0800

    Initial Setup

--- src/pages/Groups/Group1/index.tsx
+++ src/pages/Groups/Group1/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import GroupContent from '../../../components/GroupContent'
+import BookingOld from '../../../assets/Group1/booking_old.svg'
+import ScheduleOld from '../../../assets/Group1/scheduling_old.svg'
+
 const Group1 = () => {
-  return <>Group 1 page</>
+  return (
+    <>
+      <GroupContent groupNumber={1} oldScreen1ImgSrc={BookingOld} oldScreen2ImgSrc={ScheduleOld} />
+    </>
+  )
 }
 
 export default Group1
