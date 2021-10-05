98c609174698a97c98b355d6cdb3501d4e8f38da
Magmanat <princemjp@gmail.com>
2021-10-06 04:31:57 +0800

    Added more items to Successfully Deleted Event page

--- src/pages/Groups/Group1/Screen1.tsx
+++ src/pages/Groups/Group1/Screen1.tsx
@@ -3,16 +3,22 @@ import LeftOutlined from '@ant-design/icons/lib/icons/LeftOutlined'
 import UnorderedListOutlined from '@ant-design/icons/lib/icons/UnorderedListOutlined'
 import {
   SucessfullyDeletedEvent,
-  SuccessfullyDeletedContainer,
   TopNav,
   BotNav,
   Header,
   StyledButton,
+  StyledTick,
+  StyledUndo,
+  DeletedEventText,
+  EventName,
+  EventTime,
 } from './styles/Screen1.styled'
 import homeIconUnselected from '../../../assets/Group1/homeIconUnselected.svg'
 import facilitiesIconSelected from '../../../assets/Group1/facilitiesIconSelected.svg'
 import calenderIconUnselected from '../../../assets/Group1/calenderIconUnselected.svg'
 import profileIconUnselected from '../../../assets/Group1/profileIconUnselected.svg'
+import tickIconRH from '../../../assets/Group1/tickIconRH.svg'
+import undoButtonIcon from '../../../assets/Group1/undoButtonIcon.svg'
 
 const Screen1 = () => {
   return (
@@ -22,9 +28,14 @@ const Screen1 = () => {
         <Header> My Bookings </Header>
       </TopNav>
 
-      <SuccessfullyDeletedContainer>
-        <SucessfullyDeletedEvent background="lightgreen"></SucessfullyDeletedEvent>
-      </SuccessfullyDeletedContainer>
+      <SucessfullyDeletedEvent>
+        <StyledTick src={tickIconRH} />
+        <DeletedEventText>Successfully Deleted Event!</DeletedEventText>
+        <EventName>Voices Bonding Camp</EventName>
+        <EventTime>16/12/21 1:00 PM to 3:00PM</EventTime>
+        <StyledUndo src={undoButtonIcon} />
+        {/* ask design team to sketch undo button */}
+      </SucessfullyDeletedEvent>
 
       <BotNav>
         <StyledButton src={homeIconUnselected} />
--- src/pages/Groups/Group1/styles/Screen1.styled.ts
+++ src/pages/Groups/Group1/styles/Screen1.styled.ts
@@ -1,37 +1,37 @@
 import styled from 'styled-components'
 
 export const TopNav = styled.div`
-  width: 50%;
+  width: 100%;
   height: 7vh;
   padding: 1em;
   font-size: 28px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  position: fixed;
+  position: absolute;
   background: #ffffff;
 `
-export const SuccessfullyDeletedContainer = styled.div<{ fontSize?: string }>`
-  margin: 0;
-  height: 100%;
-  width: 100%;
-  color: black;
-  padding: 7vh 0px;
-  //border-style: solid;
-  white-space: nowrap;
-  ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
-`
-export const SucessfullyDeletedEvent = styled.div<{ background?: string }>`
-  margin: 15px 5%;
+export const SucessfullyDeletedEvent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 16px 26px;
+  position: absolute;
+  width: 90%;
+  margin: 15px 2%;
+  height: 200px;
+  left: 22px;
+  top: 70px;
+  background: #f6ffed;
+  /* Polar Green/3 */
+  border: 1px solid #b7eb8f;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
-  border: solid 1px;
-  padding: 30px;
-  color: black;
-  ${(props) => (props.background && props.background === 'lightgreen' ? `background: #F6FFED;` : `background: white`)}
-  white-space: nowrap;
-  min-height: 200px;
 `
 export const Header = styled.h1`
+  left: 20vh;
+  position: absolute;
   font-size: 50px;
   font-weight: bold;
   margin: 0;
@@ -54,4 +54,49 @@ export const BotNav = styled.div`
 export const StyledButton = styled.img`
   width: 28px;
   max-height: 28px;
+`
+export const StyledTick = styled.img`
+  width: 28px;
+  position: absolute;
+  left: 30px;
+  top: 25px;
+`
+export const StyledUndo = styled.img`
+  width: 28px;
+  position: absolute;
+  right: 120px;
+  bottom: 25px;
+`
+export const DeletedEventText = styled.p<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  font-family: Lato;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 22px;
+  position: absolute;
+  left: 150px;
+  color: rgba(0, 0, 0, 0.85);
+`
+export const EventName = styled.p<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  font-family: Lato;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 110px;
+  position: absolute;
+  left: 150px;
+  color: rgba(0, 0, 0, 0.65);
+`
+export const EventTime = styled.p<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  font-family: Lato;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 200px;
+  position: absolute;
+  left: 150px;
+  color: rgba(0, 0, 0, 0.65);
 `
