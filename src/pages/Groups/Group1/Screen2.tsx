819b0949b881b431b2666e01ca490d13fc62bd82
chun <lamchunyu00@gmail.com>
2021-10-05 21:28:56 +0800

    Added UI Improvements and followed Figma styles for implementation

--- src/pages/Groups/Group1/Screen2.tsx
+++ src/pages/Groups/Group1/Screen2.tsx
@@ -1,41 +1,69 @@
 import React from 'react'
-
+import LeftOutlined from '@ant-design/icons/lib/icons/LeftOutlined'
+import UnorderedListOutlined from '@ant-design/icons/lib/icons/UnorderedListOutlined'
 import {
   Bookings_container,
-  Individual_bookings_1,
-  Individual_bookings_2,
-  Individual_bookings_3,
+  IndividualBookings,
+  TopNav,
+  Header,
+  EventText,
+  LocationText,
+  BotNav,
+  StyledButton,
 } from './styles/Screen2.styled'
+import homeIconUnselected from '../../../assets/Group1/homeIconUnselected.svg'
+import facilitiesIconUnselected from '../../../assets/Group1/facilitiesIconUnselected.svg'
+import calenderIconSelected from '../../../assets/Group1/calenderIconSelected.svg'
+import profileIconUnselected from '../../../assets/Group1/profileIconUnselected.svg'
 
 const Screen2 = () => {
   return (
     <>
+      <TopNav>
+        <LeftOutlined />
+        <Header>Semester 1, Week 7</Header>
+        <UnorderedListOutlined />
+      </TopNav>
+
       <Bookings_container>
-        <Individual_bookings_1>
-          <div>Event Name</div>
-          <div>Location</div>
-        </Individual_bookings_1>
+        {/*TODO: Create new component*/}
+        <IndividualBookings background="green">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
 
-        <Individual_bookings_2>
-          <div>Event Name</div>
-          <div>Location</div>
-        </Individual_bookings_2>
+        <IndividualBookings background="white">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
 
-        <Individual_bookings_3>
-          <div>Event Name</div>
-          <div>Location</div>
-        </Individual_bookings_3>
+        <IndividualBookings background="green">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
 
-        <Individual_bookings_2>
-          <div>Event Name</div>
-          <div>Location</div>
-        </Individual_bookings_2>
+        <IndividualBookings background="white">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
 
-        <Individual_bookings_1>
-          <div>Event Name</div>
-          <div>Location</div>
-        </Individual_bookings_1>
+        <IndividualBookings background="green">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
+
+        <IndividualBookings background="white">
+          <EventText>Event Name</EventText>
+          <LocationText>Location</LocationText>
+        </IndividualBookings>
       </Bookings_container>
+      <BotNav>
+        <StyledButton src={homeIconUnselected} />
+        <StyledButton src={facilitiesIconUnselected} />
+        {/*TODO: Get designers to get a green selected svg for this icon*/}
+        <StyledButton src={calenderIconSelected} />
+        <StyledButton src={profileIconUnselected} />
+      </BotNav>
     </>
   )
 }
