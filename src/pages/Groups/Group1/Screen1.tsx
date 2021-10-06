1f13801ad90c6ecf851b026138bccd063bf1d965
Magmanat <princemjp@gmail.com>
2021-10-06 17:45:46 +0800

    added undo button and space for centre nav

--- src/pages/Groups/Group1/Screen1.tsx
+++ src/pages/Groups/Group1/Screen1.tsx
@@ -8,41 +8,45 @@ import {
   Header,
   StyledButton,
   StyledTick,
-  StyledUndo,
   DeletedEventText,
   EventName,
   EventTime,
+  UndoIcon,
+  CenterNav,
 } from './styles/Screen1.styled'
 import homeIconUnselected from '../../../assets/Group1/homeIconUnselected.svg'
 import facilitiesIconSelected from '../../../assets/Group1/facilitiesIconSelected.svg'
 import calenderIconUnselected from '../../../assets/Group1/calenderIconUnselected.svg'
 import profileIconUnselected from '../../../assets/Group1/profileIconUnselected.svg'
 import tickIconRH from '../../../assets/Group1/tickIconRH.svg'
-import undoButtonIcon from '../../../assets/Group1/undoButtonIcon.svg'
 
 const Screen1 = () => {
   return (
     <>
-      <TopNav>
-        <LeftOutlined />
-        <Header> My Bookings </Header>
-      </TopNav>
+      <div>
+        <TopNav>
+          <LeftOutlined />
+          <Header> My Bookings </Header>
+        </TopNav>
+      </div>
+      <div>
+        <SucessfullyDeletedEvent>
+          <StyledTick src={tickIconRH} />
+          <DeletedEventText>Successfully Deleted Event!</DeletedEventText>
+          <EventName>Voices Bonding Camp</EventName>
+          <EventTime>16/12/21 1:00 PM to 3:00PM</EventTime>
+          <UndoIcon>undo</UndoIcon>
+        </SucessfullyDeletedEvent>
 
-      <SucessfullyDeletedEvent>
-        <StyledTick src={tickIconRH} />
-        <DeletedEventText>Successfully Deleted Event!</DeletedEventText>
-        <EventName>Voices Bonding Camp</EventName>
-        <EventTime>16/12/21 1:00 PM to 3:00PM</EventTime>
-        <StyledUndo src={undoButtonIcon} />
-        {/* ask design team to sketch undo button */}
-      </SucessfullyDeletedEvent>
+        <CenterNav></CenterNav>
 
-      <BotNav>
-        <StyledButton src={homeIconUnselected} />
-        <StyledButton src={facilitiesIconSelected} />
-        <StyledButton src={calenderIconUnselected} />
-        <StyledButton src={profileIconUnselected} />
-      </BotNav>
+        <BotNav>
+          <StyledButton src={homeIconUnselected} />
+          <StyledButton src={facilitiesIconSelected} />
+          <StyledButton src={calenderIconUnselected} />
+          <StyledButton src={profileIconUnselected} />
+        </BotNav>
+      </div>
     </>
   )
 }
--- src/pages/Groups/Group1/styles/Screen1.styled.ts
+++ src/pages/Groups/Group1/styles/Screen1.styled.ts
@@ -17,9 +17,9 @@ export const SucessfullyDeletedEvent = styled.div`
   align-items: flex-start;
   padding: 16px 26px;
   position: absolute;
-  width: 90%;
+  width: 600px;
   margin: 15px 2%;
-  height: 200px;
+  height: 160px;
   left: 22px;
   top: 70px;
   background: #f6ffed;
@@ -61,12 +61,6 @@ export const StyledTick = styled.img`
   left: 30px;
   top: 25px;
 `
-export const StyledUndo = styled.img`
-  width: 28px;
-  position: absolute;
-  right: 120px;
-  bottom: 25px;
-`
 export const DeletedEventText = styled.p<{ fontSize?: string }>`
   ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
   font-family: Lato;
@@ -99,4 +93,37 @@ export const EventTime = styled.p<{ fontSize?: string }>`
   position: absolute;
   left: 150px;
   color: rgba(0, 0, 0, 0.65);
+`
+export const UndoIcon = styled.div<{ fontSize?: string }>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize}`}
+  color: rgba(256, 256, 256, 0.9);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: absolute;
+  align-items: center;
+  margin: 15px 10%;
+  width: 50px;
+  height: 25px;
+  right: 22px;
+  bottom: 10px;
+  background: #468751;
+  /* Polar Green/3 */
+  border: 1px solid #b7eb8f;
+  box-sizing: border-box;
+  border-radius: 5px;
+`
+export const CenterNav = styled.div`
+  width: 100%;
+  height: 7vh;
+  padding: 1em;
+  width: 595px;
+  left: 40px;
+  top: 260px;
+  font-size: 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
+  background: #ff0000;
 `
