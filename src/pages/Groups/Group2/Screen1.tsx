09d94152f0bca82072c00743c2046ed91d9a5ef9
limwenfeng <wilfred.limwf@gmail.com>
2021-10-07 23:16:53 +0800

    created css files for both screens

--- src/pages/Groups/Group2/Screen1.tsx
+++ src/pages/Groups/Group2/Screen1.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Screen1.css'
 
 const Screen1 = () => {
-  return <>this is screen 1</>
+  return (
+    <>
+      this is screen 1<h1>test</h1>
+    </>
+  )
 }
 
 export default Screen1
--- src/pages/Groups/Group2/Screen2.tsx
+++ src/pages/Groups/Group2/Screen2.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Screen2.css'
 
 const Screen2 = () => {
   return <>this is screen 2</>
