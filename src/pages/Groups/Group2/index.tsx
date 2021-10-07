77bc8bff99a4f519f9cc6f8c0b09b81d62a733a8
limwenfeng <wilfred.limwf@gmail.com>
2021-10-08 01:02:30 +0800

    added names

--- src/pages/Groups/Group2/index.tsx
+++ src/pages/Groups/Group2/index.tsx
@@ -4,8 +4,8 @@ import GroupContent from '../../../components/GroupContent'
 const Group2 = () => {
   return (
     <GroupContent
-      groupNumber={0}
-      names="name 1, name 2, ..."
+      groupNumber={2}
+      names="wenfeng, jason, yu an, drey, amos, yong bin, yong zhang"
       oldScreen1ImgSrc="https://picsum.photos/300/600"
       oldScreen2ImgSrc="https://picsum.photos/300/600"
     />
