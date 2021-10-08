029ad34ef127e84502a324671848b2195458df2a
limwenfeng <wilfred.limwf@gmail.com>
2021-10-08 13:32:05 +0800

    added old screen as png and imported into group page

--- src/pages/Groups/Group2/index.tsx
+++ src/pages/Groups/Group2/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import GroupContent from '../../../components/GroupContent'
+import OldScreen1 from '../../../assets/Group1/OldScreen1.png'
 
 const Group2 = () => {
   return (
     <GroupContent
       groupNumber={2}
       names="wenfeng, jason, yu an, drey, amos, yong bin, yong zhang"
-      oldScreen1ImgSrc="https://picsum.photos/300/600"
+      oldScreen1ImgSrc={OldScreen1}
       oldScreen2ImgSrc="https://picsum.photos/300/600"
     />
   )
