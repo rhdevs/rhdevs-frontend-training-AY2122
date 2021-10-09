3bbb1127f63101345d2aa872ea0f3544eab9ac73
shaokiat <shaokiat@gmail.com>
2021-10-09 17:09:19 +0800

    Skeleton for Screen 1

--- src/pages/Groups/Group2/Screen1.tsx
+++ src/pages/Groups/Group2/Screen1.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import { MainContainer, Screen, BottomNavBar } from './styles/Screen1.styled'
+import {
+  MainContainer,
+  Screen,
+  BottomNavBar,
+  SearchBar,
+  SearchButton,
+  TextSection,
+  Title,
+  ButtonsSection,
+  PostsSection,
+  FilterBar,
+  Post,
+} from './styles/Screen1.styled'
 
 const Screen1 = () => {
   return (
     <>
       <MainContainer>
         <Screen>
-          Build new screen here.
+          <SearchBar>
+            Good Afternoon Mao Mao,
+            <SearchButton>Icon</SearchButton>
+          </SearchBar>
+          <TextSection>
+            <Title>Meow Meow Porridge</Title>
+            <div style={{ textAlign: 'center' }}>Text text text...</div>
+          </TextSection>
+          <ButtonsSection>Buttons</ButtonsSection>
+          <PostsSection>
+            {"What's Happening..."}
+            <FilterBar>FilterBar</FilterBar>
+            <Post>Post1</Post>
+            <Post>Post2</Post>
+          </PostsSection>
+
           <BottomNavBar>Navigation Bar</BottomNavBar>
         </Screen>
       </MainContainer>
--- src/pages/Groups/Group2/styles/Screen1.styled.ts
+++ src/pages/Groups/Group2/styles/Screen1.styled.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const MainContainer = styled.div`
-  font-size: 2rem;
+  font-size: 1.5rem;
   border: 1px black;
   display: flex;
   flex-direction: row;
@@ -16,8 +16,76 @@ export const Screen = styled.div`
   height: 100vh;
 `
 
-export const BottomNavBar = styled.div`
+export const SearchBar = styled.div`
+  position: flex;
+  top: 0;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 70px;
+  border: 1px solid green;
+  border-radius: 0 0 15px 15px;
+`
+
+export const SearchButton = styled.div`
   position: fixed;
+  top: 0;
+  right: 38%; /*Not too sure about this*/
+  background-color: blue;
+`
+
+export const TextSection = styled.div`
+  position: flex;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 10vw;
+  background-color: lightblue;
+  border: 1px solid green;
+`
+
+export const Title = styled.div`
+  font-size: 2rem;
+  text-align: center;
+`
+
+export const ButtonsSection = styled.div`
+  position: flex;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 5vw;
+  border: 1px solid green;
+`
+
+export const PostsSection = styled.div`
+  position: flex;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 20vw;
+  border: 1px solid green;
+`
+
+export const FilterBar = styled.div`
+  position: flex;
+  text-align: center;
+  background-color: lightgreen;
+`
+// would it be better to abstract this Post into another component?
+export const Post = styled.div`
+  position: flex;
+  max-width: 350px;
+  min-width: 250px;
+  width: 40vw;
+  height: 5vw;
+  border: 1px solid green;
+  margin: auto;
+  margin-bottom: 10px;
+`
+
+export const BottomNavBar = styled.div`
+  position: flex;
   bottom: 0;
   max-width: 400px;
   min-width: 300px;
