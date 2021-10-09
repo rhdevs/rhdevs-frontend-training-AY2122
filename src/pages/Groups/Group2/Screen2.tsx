d889d590d3e20f67740c8ab9e4b2b0dd91f3f73c
jason-ng-zq99 <jason.ng.zq99@gmail.com>
2021-10-09 15:13:21 +0800

    Add containers for smaller components

--- src/pages/Groups/Group2/Screen2.tsx
+++ src/pages/Groups/Group2/Screen2.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import { MainContainer, Screen } from './styles/Screen1.styled'
+import {
+  MainContainer,
+  Screen,
+  Picture,
+  BookingTitle,
+  DurationAndAuthor,
+  Timing,
+  Notes,
+  ConfirmButton,
+  BottomNavBar,
+} from './styles/Screen2.styled'
 
 const Screen2 = () => {
   return (
     <>
       <MainContainer>
-        <Screen>Build new screen here.</Screen>
+        <Screen>
+          <Picture>Venue picture.</Picture>
+          <BookingTitle>Booking title.</BookingTitle>
+          <DurationAndAuthor>Booking duration and author.</DurationAndAuthor>
+          <Timing>Timing of booking.</Timing>
+          <Notes>Additional notes of bookings</Notes>
+          <ConfirmButton>Confirm button.</ConfirmButton>
+          <BottomNavBar>Bottom nav bar.</BottomNavBar>
+        </Screen>
       </MainContainer>
     </>
   )
--- src/pages/Groups/Group2/styles/Screen2.styled.ts
+++ src/pages/Groups/Group2/styles/Screen2.styled.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+const bookingTitleTop = 25
+const durationAndAuthorTop = 35
+const timingTop = durationAndAuthorTop + 15
+const notesTop = timingTop + 15
+const confirmButtonTop = notesTop + 15
+
 export const MainContainer = styled.div`
   font-size: 2rem;
   border: 1px black;
@@ -14,4 +20,78 @@ export const Screen = styled.div`
   min-width: 300px;
   width: 50vw;
   height: 100vh;
+`
+
+export const Picture = styled.div`
+  position: fixed;
+  top: 0;
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 35vh;
+  text-align: center;
+`
+export const BookingTitle = styled.div`
+  position: fixed;
+  top: ${bookingTitleTop + 'vh'};
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 10vh;
+  text-align: center;
+`
+export const DurationAndAuthor = styled.div`
+  position: fixed;
+  top: ${durationAndAuthorTop + 'vh'};
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 15vh;
+  text-align: center;
+`
+export const Timing = styled.div`
+  position: fixed;
+  top: ${timingTop + 'vh'};
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 15vh;
+  text-align: center;
+`
+
+export const Notes = styled.div`
+  position: fixed;
+  top: ${notesTop + 'vh'};
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 15vh;
+  text-align: center;
+`
+
+export const ConfirmButton = styled.div`
+  position: fixed;
+  top: ${confirmButtonTop + 'vh'};
+  border: 1px solid red;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 15vh;
+  text-align: center;
+`
+
+export const BottomNavBar = styled.div`
+  position: fixed;
+  bottom: 0;
+  max-width: 400px;
+  min-width: 300px;
+  width: 50vw;
+  height: 50px;
+  text-align: center;
+  border: 1px solid red;
 `
