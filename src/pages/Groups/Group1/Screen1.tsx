dfd6349c252b675889d2cf68d484fef024bff323
Maxim Yam <maximyam@gmail.com>
2021-10-09 10:15:59 +0800

    add delete sortby and bandroom

--- src/pages/Groups/Group1/Screen1.tsx
+++ src/pages/Groups/Group1/Screen1.tsx
@@ -13,12 +13,28 @@ import {
   EventTime,
   UndoIcon,
   CenterNav,
+  GreyBoxDelete,
+  GreyBoxSort,
+  GreyBoxText,
+  StyledGreybox,
+  Booking,
+  BookingLocation,
+  BookingCCA,
+  BookingTiming,
+  StyledEdit,
+  StyledDustbin,
+  StyledCCAIcon,
 } from './styles/Screen1.styled'
 import homeIconUnselected from '../../../assets/Group1/homeIconUnselected.svg'
 import facilitiesIconSelected from '../../../assets/Group1/facilitiesIconSelected.svg'
 import calenderIconUnselected from '../../../assets/Group1/calenderIconUnselected.svg'
 import profileIconUnselected from '../../../assets/Group1/profileIconUnselected.svg'
 import tickIconRH from '../../../assets/Group1/tickIconRH.svg'
+import dustbinIconBlack from '../../../assets/Group1/dustbinIconBlack.svg'
+import Schedule from '../../../assets/Group1/Schedule.svg'
+import greendustbin from '../../../assets/Group1/dustbinIconGreen.svg'
+import greenedit from '../../../assets/Group1/editIconGreen.svg'
+import bandroom from '../../../assets/Group1/bandroomIcon.svg'
 
 const Screen1 = () => {
   return (
@@ -39,6 +55,24 @@ const Screen1 = () => {
         </SucessfullyDeletedEvent>
 
         <CenterNav></CenterNav>
+
+        <GreyBoxDelete>
+          <GreyBoxText>Recently Deleted</GreyBoxText>
+          <StyledGreybox src={dustbinIconBlack} />
+        </GreyBoxDelete>
+        <GreyBoxSort>
+          <GreyBoxText>Sort By</GreyBoxText>
+          <StyledGreybox src={Schedule} />
+        </GreyBoxSort>
+
+        <Booking>
+          <StyledCCAIcon src={bandroom} />
+          <BookingLocation>Bandroom</BookingLocation>
+          <BookingCCA> Rhebels Practice</BookingCCA>
+          <BookingTiming> 16/01/20 12:00 PM - 1:00 PM </BookingTiming>
+          <StyledEdit src={greenedit} />
+          <StyledDustbin src={greendustbin} />
+        </Booking>
 
         <BotNav>
           <StyledButton src={homeIconUnselected} />
