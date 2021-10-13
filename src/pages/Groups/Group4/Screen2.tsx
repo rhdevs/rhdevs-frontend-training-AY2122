09dc804c5383ec582440559ecc76beea07f800f7
Zhang Zhikai <zhangzhikai71@gmail.com>
2021-10-13 15:33:49 +0800

    moved styling for screen 2 forget password screen to seperate style file

--- src/pages/Groups/Group4/Screen2.tsx
+++ src/pages/Groups/Group4/Screen2.tsx
@@ -1,55 +1,8 @@
 import React from 'react'
+// import 'antd/dist/antd.css'
 
-import styled from 'styled-components'
-// import TopNavBar from '../../../components/Mobile/TopNavBar'
-// import { Alert, Button, Input } from 'antd'
-import 'antd/dist/antd.css'
-
+import { ContainerDiv, ForgetPasswordContainer, Logo, Link } from './styles/Screen2.styled'
 import logo from '../../../assets/group4/devsLogo.svg'
-
-const ContainerDiv = styled.div`
-  background-color: #ffffff;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const ForgetPasswordContainer = styled.div`
-  height: 100vh;
-  width: 100vw;
-  margin: 0px 23px;
-  padding-top: 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const Logo = styled.img`
-  width: 70%;
-  max-width: 255px;
-  max-height: 255px;
-  margin-top: 20vh;
-`
-
-const Link = styled.a`
-  width: 370px;
-  height: 19px;
-  left: 22px;
-  top: 467px;
-
-  font-family: Lato;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 19px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  letter-spacing: -0.015em;
-
-  color: #59ba95;
-`
 
 const ForgotPasswordPage = () => {
   return (
---
+++ src/pages/Groups/Group4/styles/Screen2.styled.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const ContainerDiv = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ForgetPasswordContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  margin: 0px 23px;
+  padding-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  width: 70%;
+  max-width: 255px;
+  max-height: 255px;
+  margin-top: 20vh;
+`
+
+export const Link = styled.a`
+  width: 370px;
+  height: 19px;
+  left: 22px;
+  top: 467px;
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  letter-spacing: -0.015em;
+  color: #59ba95;
+`
