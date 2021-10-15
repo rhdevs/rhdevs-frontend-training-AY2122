17cacb08a401888759ba9abb245f13b0c904a55c
Maxim Yam <maximyam@gmail.com>
2021-10-15 20:18:20 +0800

    add Table for quantity itemname and action

--- src/pages/Pair2/index.tsx
+++ src/pages/Pair2/index.tsx
@@ -1,7 +1,69 @@
 import React from 'react'
+import {
+  ShoppingListHeader,
+  GroupCardsContainer,
+  FullScreenContainer,
+  Quantity,
+  ItemName,
+  Action,
+  QuantityContainer,
+  ItemNameContainer,
+  ActionContainer,
+} from './styles/Pair2.styled'
 
 const Pair2 = () => {
-  return <>example shopping list</>
+  return (
+    <>
+      <ShoppingListHeader> Shopping List</ShoppingListHeader>
+      <FullScreenContainer>
+        <GroupCardsContainer>
+          <QuantityContainer>
+            <Quantity>Quantity</Quantity>
+          </QuantityContainer>
+          <ItemNameContainer>
+            <ItemName>Item Name</ItemName>
+          </ItemNameContainer>
+          <ActionContainer>
+            <Action>Action</Action>
+          </ActionContainer>
+        </GroupCardsContainer>
+
+        <GroupCardsContainer>
+          <QuantityContainer>
+            <Quantity>3</Quantity>
+          </QuantityContainer>
+          <ItemNameContainer>
+            <ItemName>Eggs</ItemName>
+          </ItemNameContainer>
+          <ActionContainer>
+            <Action>Delete</Action>
+          </ActionContainer>
+        </GroupCardsContainer>
+        <GroupCardsContainer>
+          <QuantityContainer>
+            <Quantity>3</Quantity>
+          </QuantityContainer>
+          <ItemNameContainer>
+            <ItemName>Eggs</ItemName>
+          </ItemNameContainer>
+          <ActionContainer>
+            <Action>Delete</Action>
+          </ActionContainer>
+        </GroupCardsContainer>
+        <GroupCardsContainer>
+          <QuantityContainer>
+            <Quantity>3</Quantity>
+          </QuantityContainer>
+          <ItemNameContainer>
+            <ItemName>Eggs</ItemName>
+          </ItemNameContainer>
+          <ActionContainer>
+            <Action>Delete</Action>
+          </ActionContainer>
+        </GroupCardsContainer>
+      </FullScreenContainer>
+    </>
+  )
 }
 
 export default Pair2
--- src/pages/Pair2/styles/Pair2.styled.ts
+++ src/pages/Pair2/styles/Pair2.styled.ts
@@ -5,14 +5,53 @@ export const ShoppingListHeader = styled.h1`
   font-weight: 400;
   margin: 20px auto;
 `
-
+export const FullScreenContainer = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  position: static;
+  width: 100%;
+  height: 100%;
+  padding: 1em;
+`
 export const GroupCardsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 300px);
-  grid-auto-rows: 200px;
+  grid-template-columns: 3fr 5fr 3fr;
   grid-gap: 1rem;
   justify-content: space-evenly;
   padding: 10px 0;
   width: 90vw;
   margin: auto;
+  flex-direction: row;
+  padding: 1em;
+`
+export const QuantityContainer = styled.div`
+  position: static;
+  padding: 1px;
+`
+export const Quantity = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: calc(11px + 1vw);
+  position: relative;
+`
+export const ItemNameContainer = styled.div`
+  position: static;
+  padding: 1px;
+`
+export const ItemName = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: calc(11px + 1vw);
+  position: relative;
+`
+export const ActionContainer = styled.div`
+  position: static;
+  padding: 1px;
+`
+export const Action = styled.p`
+  font-style: normal;
+  font-weight: normal;
+  font-size: calc(11px + 1vw);
+  position: relative;
 `
