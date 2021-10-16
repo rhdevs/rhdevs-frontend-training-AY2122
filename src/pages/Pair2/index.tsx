aa56716f22dfc5a5a9b6b40d60f4f9405cb99699
Maxim Yam <maximyam@gmail.com>
2021-10-17 00:44:57 +0800

    add action increasing and decreasing quantity

--- src/pages/Pair2/index.tsx
+++ src/pages/Pair2/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   ShoppingListHeader,
   GroupCardsContainer,
@@ -10,8 +10,31 @@ import {
   ItemNameContainer,
   ActionContainer,
 } from './styles/Pair2.styled'
+import { Button } from 'antd'
 
-const Pair2 = () => {
+export default function Pair2() {
+  const [Pair2a, setPair2number] = useState(3)
+  const handleIncrease = () => {
+    setPair2number(Pair2a + 1)
+  }
+  const handleDecrease = () => {
+    setPair2number(Pair2a - 1)
+  }
+  const [Pair2b, setPair2numberb] = useState(3)
+  const handleIncreaseb = () => {
+    setPair2numberb(Pair2b + 1)
+  }
+  const handleDecreaseb = () => {
+    setPair2numberb(Pair2b - 1)
+  }
+  const [Pair2c, setPair2numberc] = useState(3)
+  const handleIncreasec = () => {
+    setPair2numberc(Pair2c + 1)
+  }
+  const handleDecreasec = () => {
+    setPair2numberc(Pair2c - 1)
+  }
+
   return (
     <>
       <ShoppingListHeader> Shopping List</ShoppingListHeader>
@@ -27,43 +50,52 @@ const Pair2 = () => {
             <Action>Action</Action>
           </ActionContainer>
         </GroupCardsContainer>
-
         <GroupCardsContainer>
           <QuantityContainer>
-            <Quantity>3</Quantity>
+            <Quantity>{`${Pair2a}`}</Quantity>
           </QuantityContainer>
           <ItemNameContainer>
             <ItemName>Eggs</ItemName>
           </ItemNameContainer>
           <ActionContainer>
-            <Action>Delete</Action>
+            <Action>
+              Delete
+              <Button onClick={handleIncrease}>+</Button>
+              <Button onClick={handleDecrease}>-</Button>
+            </Action>
           </ActionContainer>
         </GroupCardsContainer>
         <GroupCardsContainer>
           <QuantityContainer>
-            <Quantity>3</Quantity>
+            <Quantity>{`${Pair2b}`}</Quantity>
           </QuantityContainer>
           <ItemNameContainer>
             <ItemName>Eggs</ItemName>
           </ItemNameContainer>
           <ActionContainer>
-            <Action>Delete</Action>
+            <Action>
+              Delete
+              <Button onClick={handleIncreaseb}>+</Button>
+              <Button onClick={handleDecreaseb}>-</Button>
+            </Action>
           </ActionContainer>
         </GroupCardsContainer>
         <GroupCardsContainer>
           <QuantityContainer>
-            <Quantity>3</Quantity>
+            <Quantity>{`${Pair2c}`}</Quantity>
           </QuantityContainer>
           <ItemNameContainer>
             <ItemName>Eggs</ItemName>
           </ItemNameContainer>
           <ActionContainer>
-            <Action>Delete</Action>
+            <Action>
+              Delete
+              <Button onClick={handleIncreasec}>+</Button>
+              <Button onClick={handleDecreasec}>-</Button>
+            </Action>
           </ActionContainer>
         </GroupCardsContainer>
       </FullScreenContainer>
     </>
   )
 }
-
-export default Pair2
