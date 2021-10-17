0c8f19f2ac8e0d30f2cb59ace0ab344e46541a78
Jason <jason.ng.zq99@gmail.com>
2021-10-17 19:37:21 +0800

    Add names to group card

--- src/pages/ShoppingListsMain/index.tsx
+++ src/pages/ShoppingListsMain/index.tsx
@@ -11,7 +11,7 @@ const ShoppingListMain = () => {
     <>
       <ShoppingListHeader>Our Shopping Lists!</ShoppingListHeader>
       <GroupCardsContainer>
-        <ShoppingCard names="name1, name2, name4" onClick={() => history.push(PATHS.EXAMPLE_SHOPPING_PAGE)} />
+        <ShoppingCard names="Jason, Yong Bin" onClick={() => history.push(PATHS.EXAMPLE_SHOPPING_PAGE)} />
         {/* Create group cards here */}
       </GroupCardsContainer>
     </>
