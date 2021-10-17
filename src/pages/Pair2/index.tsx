d3981d1ada9a8dc308e0581f2267ba032b37d3bf
Marcus Ong <marcus.ong.qy@gmail.com>
2021-10-17 17:56:31 +0800

    made shopping list more dynamic

--- src/pages/Pair2/index.tsx
+++ src/pages/Pair2/index.tsx
@@ -1,66 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'antd'
 import {
   ShoppingListHeader,
   GroupCardsContainer,
   FullScreenContainer,
   Quantity,
   ItemName,
-  Action,
   QuantityContainer,
   ItemNameContainer,
   ActionContainer,
 } from './styles/Pair2.styled'
 
+type Props = {
+  list: listEntry[]
+  setList: React.Dispatch<React.SetStateAction<listEntry[]>>
+  index: number
+}
+
+type listEntry = {
+  quantity: number
+  itemName: string
+}
+
+const ListItem = (props: Props) => {
+  const list = props.list
+  const setList = props.setList
+  const index = props.index
+
+  const incQuantity = () => {
+    const newList = [...list]
+    newList[index].quantity = newList[index].quantity + 1
+    setList(newList)
+  }
+
+  const decQuantity = () => {
+    const newList = [...list]
+    newList[index].quantity = newList[index].quantity - 1
+    setList(newList)
+  }
+
+  return (
+    <GroupCardsContainer>
+      <QuantityContainer>
+        <Quantity>{list[index].quantity}</Quantity>
+      </QuantityContainer>
+      <ItemNameContainer>
+        <ItemName>{list[index].itemName}</ItemName>
+      </ItemNameContainer>
+      <ActionContainer>
+        <Button onClick={incQuantity}>Increase</Button>
+        <Button onClick={decQuantity}>Decrease</Button>
+      </ActionContainer>
+    </GroupCardsContainer>
+  )
+}
+
 const Pair2 = () => {
+  const itemsList: listEntry[] = [
+    {
+      quantity: 2,
+      itemName: 'broccoli',
+    },
+    {
+      quantity: 6,
+      itemName: 'nutella',
+    },
+    {
+      quantity: 4,
+      itemName: 'grapes',
+    },
+    {
+      quantity: 6,
+      itemName: 'oranges',
+    },
+  ]
+  const [items, setItems] = useState(itemsList)
   return (
     <>
       <ShoppingListHeader> Shopping List</ShoppingListHeader>
       <FullScreenContainer>
-        <GroupCardsContainer>
-          <QuantityContainer>
-            <Quantity>Quantity</Quantity>
-          </QuantityContainer>
-          <ItemNameContainer>
-            <ItemName>Item Name</ItemName>
-          </ItemNameContainer>
-          <ActionContainer>
-            <Action>Action</Action>
-          </ActionContainer>
-        </GroupCardsContainer>
-
-        <GroupCardsContainer>
-          <QuantityContainer>
-            <Quantity>3</Quantity>
-          </QuantityContainer>
-          <ItemNameContainer>
-            <ItemName>Eggs</ItemName>
-          </ItemNameContainer>
-          <ActionContainer>
-            <Action>Delete</Action>
-          </ActionContainer>
-        </GroupCardsContainer>
-        <GroupCardsContainer>
-          <QuantityContainer>
-            <Quantity>3</Quantity>
-          </QuantityContainer>
-          <ItemNameContainer>
-            <ItemName>Eggs</ItemName>
-          </ItemNameContainer>
-          <ActionContainer>
-            <Action>Delete</Action>
-          </ActionContainer>
-        </GroupCardsContainer>
-        <GroupCardsContainer>
-          <QuantityContainer>
-            <Quantity>3</Quantity>
-          </QuantityContainer>
-          <ItemNameContainer>
-            <ItemName>Eggs</ItemName>
-          </ItemNameContainer>
-          <ActionContainer>
-            <Action>Delete</Action>
-          </ActionContainer>
-        </GroupCardsContainer>
+        {itemsList.map((value, index) => (
+          <ListItem key={`list-item-${index}`} list={items} setList={setItems} index={index} />
+        ))}
       </FullScreenContainer>
     </>
   )
