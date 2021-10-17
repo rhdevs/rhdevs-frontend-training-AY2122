48392add7c11fcc006a8ead2b73d6eb1c9864815
Jason <jason.ng.zq99@gmail.com>
2021-10-17 21:54:28 +0800

    Adjust positioning of button and header

--- src/pages/JasonYbShoppingListPage/styles/JasonYbShoppingListPage.styled.ts
+++ src/pages/JasonYbShoppingListPage/styles/JasonYbShoppingListPage.styled.ts
@@ -1,22 +1,22 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
-  display: flex;
   width: 100%;
 `
 
 export const ShoppingListHeader = styled.div`
   text-align: center;
-  align-self: center;
   font-weight: 400;
-  margin: 20px auto;
+  margin: 20px;
   font-size: 48px;
+  position: relative;
+  z-index: 1;
 `
 
 export const AddItemButtonContainer = styled.div`
   float: right;
-  align-self: center;
   margin-right: 3vw;
+  z-index: 100;
 `
 export const ShoppingListContainer = styled.div`
   display: flex;
