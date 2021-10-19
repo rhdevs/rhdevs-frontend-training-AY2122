f1cfa7c94e186039b98fbded747034fe27f4009b
Jia Yixuan <jyxhazcake@yahoo.com>
2021-10-19 17:51:20 +0800

    Added ability to add item to table

--- src/pages/Shopping_YXWP/index.tsx
+++ src/pages/Shopping_YXWP/index.tsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import { Button, Card, Input, Table } from 'antd'
+import React, { useState } from 'react'
+import { Button, Card, Input, InputNumber, Table } from 'antd'
+import { valueType } from 'antd/lib/statistic/utils'
 
-const { Column, ColumnGroup } = Table
+const { Column } = Table
 
 const Shopping_YXWP = () => {
-  const data = [
+  interface Props {
+    key: number
+    itemQuantity: valueType
+    itemName: string
+  }
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState<valueType>(0)
+  const [index, setIndex] = useState(3)
+  const [data, setData] = useState<Props[]>([
     {
-      key: '1',
-      quantity: '3',
-      itemname: 'Eggs',
-      action: 'delete',
+      key: 1,
+      itemQuantity: 3,
+      itemName: 'Dummy Item 1',
     },
     {
-      key: '2',
-      quantity: '4',
-      itemname: 'Eggs',
-      action: 'delete',
+      key: 2,
+      itemQuantity: 4,
+      itemName: 'Dummy Item 2',
     },
-  ]
+  ])
+
+  const handleAddItem = () => {
+    setData((prevData: Props[]) => [...prevData, { key: index, itemQuantity: quantity, itemName: name }])
+    setIndex(index + 1)
+  }
 
   return (
     <>
       <Card>
         <h1>Shopping time! whee! To be updated!</h1>
-        <Input placeholder="yes" />
-        <Button type="primary">Add item</Button>
+        <Input placeholder="Item Name" onChange={(e) => setName(e.target.value)} />
+        <InputNumber placeholder="Quantity" onChange={(e) => setQuantity(e)} />
+        <Button type="primary" onClick={handleAddItem}>
+          Add item
+        </Button>
         <Table dataSource={data}>
-          <Column title="Quantity" dataIndex="quantity" key="quantity" />
-          <Column title="Item Name" dataIndex="itemname" key="itemname" />
+          <Column title="Index" dataIndex="key" key="key" />
+          <Column title="Quantity" dataIndex="itemQuantity" key="itemQuantity" />
+          <Column title="Item Name" dataIndex="itemName" key="itemName" />
           <Column title="Action" dataIndex="action" key="action" render={() => <Button> To be updated </Button>} />
         </Table>
       </Card>
