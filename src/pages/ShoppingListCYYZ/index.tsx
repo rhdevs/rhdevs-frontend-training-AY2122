3562b38b2ea1c2fce5cea79e32d64fc30d4d4c26
chun <lamchunyu00@gmail.com>
2021-10-19 17:45:00 +0800

    Made basic UI and layout

--- src/pages/ShoppingListCYYZ/index.tsx
+++ src/pages/ShoppingListCYYZ/index.tsx
@@ -1,13 +1,62 @@
 import React from 'react'
-
-import { ShoppingListHeader } from './styles/ShoppingListCYYZ.styled'
+import { PageHeader, Button, Card } from 'antd'
+import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined'
+import MinusOutlined from '@ant-design/icons/lib/icons/MinusOutlined'
+import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined'
+import {
+  MainContainer,
+  BigHeader,
+  TableBody,
+  ListItem,
+  ItemName,
+  ItemAction,
+  AddContainer,
+} from './styles/ShoppingListCYYZ.styled'
 
 const ShoppingListMain = () => {
   return (
-    <>
-      <ShoppingListHeader>Our Shopping Lists!</ShoppingListHeader>
-    </>
+    <MainContainer>
+      <BigHeader>
+        <PageHeader onBack={() => null} title="Shopping List" />
+        <AddContainer>
+          <Button>Add Item</Button>
+        </AddContainer>
+      </BigHeader>
+
+      <TableBody>
+        <Card title="Food Items" style={style.card}>
+          {FoodItems.map((e) => (
+            <ListItem key={e.name}>
+              <ItemName>{e.name}</ItemName>
+              <ItemAction>
+                <Button shape="circle" icon={<MinusOutlined />} />
+                {e.amount}
+                <Button shape="circle" icon={<PlusOutlined />} />
+                <Button shape="circle" icon={<DeleteOutlined />} />
+              </ItemAction>
+            </ListItem>
+          ))}
+        </Card>
+        <Card title="Others" style={style.card} />
+      </TableBody>
+    </MainContainer>
   )
 }
+
+const FoodItems = [
+  { name: 'Egg', amount: 1 },
+  { name: 'Milk', amount: 2 },
+]
+
+const categories = ['Food Items', 'Others']
+
+const style = {
+  card: {
+    borderRadius: '10px',
+    marginBottom: '30px',
+  },
+}
+
+type Item = { name: string; amount: number }
 
 export default ShoppingListMain
--- src/pages/ShoppingListCYYZ/styles/ShoppingListCYYZ.styled.ts
+++ src/pages/ShoppingListCYYZ/styles/ShoppingListCYYZ.styled.ts
@@ -1,7 +1,51 @@
 import styled from 'styled-components'
 
-export const ShoppingListHeader = styled.h1`
-  text-align: center;
-  font-weight: 400;
-  margin: 20px auto;
+export const MainContainer = styled.div`
+  margin: 15px 5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const BigHeader = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 10px;
+  border: 1px solid rgb(235, 237, 240);
+`
+
+export const AddContainer = styled.div`
+  margin: 0 24px 0 0;
+`
+
+export const TableBody = styled.div`
+  width: 100%;
+  //min-height: 500px;
+  margin-top: 30px;
+  border-radius: 10px;
+`
+
+export const ItemName = styled.div`
+  margin: 0 15px;
+`
+
+export const ItemAction = styled.div`
+  margin: 0 15px;
+  width: 100%;
+  max-width: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ListItem = styled.div`
+  width: 100%;
+  height: 8vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid rgb(235, 237, 240);
 `
