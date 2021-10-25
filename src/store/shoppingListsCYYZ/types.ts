f98d387cf923bfa306c0e027d5c9f2e07c487138
chun <lamchunyu00@gmail.com>
2021-10-25 23:51:32 +0800

    Moved logic to reducer

--- src/store/shoppingListsCYYZ/types.ts
+++ src/store/shoppingListsCYYZ/types.ts
@@ -42,32 +42,32 @@ type SetOthersList = {
 
 type AddFoodAmount = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.ADD_FOOD_AMOUNT
-  update: Item
+  update: Item[]
 }
 
 type MinusFoodAmount = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.MINUS_FOOD_AMOUNT
-  update: Item
+  update: Item[]
 }
 
 type DeleteFoodAmount = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.DELETE_FOOD_ITEM
-  update: Item
+  update: Item[]
 }
 
 type AddOthersAmount = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.ADD_OTHERS_AMOUNT
-  update: Item
+  update: Item[]
 }
 
 type MinusOthersAmount = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.MINUS_OTHERS_AMOUNT
-  update: Item
+  update: Item[]
 }
 
 type DeleteOthersItem = {
   type: typeof SHOPPING_LIST_ACTIONS_CYYZ.DELETE_OTHERS_ITEM
-  update: Item
+  update: Item[]
 }
 
 export type ActionTypes =
