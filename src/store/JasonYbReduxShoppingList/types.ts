7030d3965bbd88bc86255cdc9d821485f1f8248e
Jason <jason.ng.zq99@gmail.com>
2021-10-25 16:51:21 +0800

    Adds IncrementKeyCount and IncrementIndexCount to types

--- src/store/JasonYbReduxShoppingList/types.ts
+++ src/store/JasonYbReduxShoppingList/types.ts
@@ -11,12 +11,24 @@ export type ShoppingItem = {
 export enum SHOPPING_LIST_ACTIONS {
   //follow naming convention <enum name>.<enum item name>
   SET_ITEM_LIST = 'SHOPPING_LIST_ACTIONS.SET_ITEM_LIST',
+  INCREMENT_KEY_COUNT = 'SHOPPING_LIST_ACTIONS.INCREMENT_KEY_COUNT',
+  INCREMENT_INDEX_COUNT = 'SHOPPING_LIST_ACTIONS.INCREMENT_INDEX_COUNT',
 }
 
 /** Actions */
 type SetItemList = {
   type: typeof SHOPPING_LIST_ACTIONS.SET_ITEM_LIST
   itemList: ShoppingItem[]
+}
+
+type IncrementKeyCount = {
+  type: typeof SHOPPING_LIST_ACTIONS.INCREMENT_KEY_COUNT
+  keyCount: number
+}
+
+type IncrementIndexCount = {
+  type: typeof SHOPPING_LIST_ACTIONS.INCREMENT_INDEX_COUNT
+  indexCount: number
 }
 
 export type ActionTypes = SetItemList
