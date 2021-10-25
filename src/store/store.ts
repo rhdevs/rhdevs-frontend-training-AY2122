53562df45de2fa8e77c194e4a9efa1627749ceb6
Tai <kktai1512@outlook.com>
2021-10-25 21:04:42 +0800

    changed store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,10 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { shoppingLists } from './shoppingLists/reducer'
+//import { shoppingLists } from './shoppingLists/reducer'
+import { AnimeGangRedux } from './AnimeGangRedux/reducer'
 
 export const rootReducer = combineReducers({
-  shoppingLists,
+  AnimeGangRedux,
 })
 const middlewares = [thunk]
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
