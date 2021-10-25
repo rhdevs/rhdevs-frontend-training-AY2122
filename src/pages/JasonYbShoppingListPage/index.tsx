0687f62111d6cb24338e9f5eeed99c978b4a83df
Jason <jason.ng.zq99@gmail.com>
2021-10-25 16:14:24 +0800

    Remove unnecessary import

--- src/pages/JasonYbShoppingListPage/index.tsx
+++ src/pages/JasonYbShoppingListPage/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { initialState } from '../../store/JasonYbReduxShoppingList/reducer'
 import { AddItemQuantity } from '../../store/JasonYbReduxShoppingList/actions'
 import { RootState } from '../../store/types'
 
