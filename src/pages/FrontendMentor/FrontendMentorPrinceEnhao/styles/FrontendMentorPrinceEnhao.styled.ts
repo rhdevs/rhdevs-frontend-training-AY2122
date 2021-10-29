d0c53dcd6da93054799386d657cf5af8720ac613
Magmanat <princemjp@gmail.com>
2021-10-29 21:21:54 +0800

    made a base layout, nothing special done

---
+++ src/pages/FrontendMentor/FrontendMentorPrinceEnhao/styles/FrontendMentorPrinceEnhao.styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1rem 3rem;
+`
+
+export const StyledHeader = styled.h1`
+  color: rgb(25, 25, 121);
+  font-family: Arial, sans-serif;
+  margin: 1rem auto;
+`
+
+export const StyledParagraph = styled.p`
+  text-align: justify;
+`
+
+export const StyledSubheader = styled.h2`
+  margin: 1rem 0 0 0;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const StyledImg = styled.img`
+  border-radius: 10px;
+`
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -51,6 +51,10 @@ const ShoppingCart4 = React.lazy(() => import(/* webpackChunckName: "ShoppingCar
 const ShoppingCart4Redux = React.lazy(
   () => import(/* webpackChunckName: "ShoppingCart4Redux" */ '../pages/ShoppingCart4Redux'),
 )
+const FrontendMentorPrinceEnhao = React.lazy(
+  () =>
+    import(/* webpackChunckName: "FrontendMentorPrinceEnhao" */ '../pages/FrontendMentor/FrontendMentorPrinceEnhao'),
+)
 
 export const Routes = () => (
   <Switch>
@@ -76,6 +80,7 @@ export const Routes = () => (
     <RouteWithTopNav exact path={PATHS.FRONTEND_MENTOR} component={FrontendMentorLanding} />
     {/* example from lesson 1 (4oct) */}
     <Route exact path="/example" component={Lesson1Example} />
+    <Route exact path={PATHS.FRONTEND_MENTOR_PRINCE_ENHAO} component={FrontendMentorPrinceEnhao} />
     <RouteWithBothNav exact path="/" component={NotFound} /> {/* fallback */}
     <Redirect to="/" />
   </Switch>
