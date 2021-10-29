6c8da9280ebac782fd1aa8191955502fcfe30f6c
Maxim Yam <maximyam@gmail.com>
2021-10-29 21:02:14 +0800

    added title description url

--- src/pages/FrontendMentor/index.tsx
+++ src/pages/FrontendMentor/index.tsx
@@ -11,8 +11,12 @@ const FrontendMentor = () => {
         title="Frontend Mentor Practices"
         description={
           <>
-            {'frontend mentor practices, challenges taken from '}
-            <a target="_blank" rel="noopener noreferrer" href={'https://frontendmentor.io'}>
+           {'frontend mentor practices, challenges taken from '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://www.frontendmentor.io/challenges/3column-preview-card-component-pH92eAR2-'}
+            >
               frontendmentor.io
             </a>
           </>
@@ -21,10 +25,9 @@ const FrontendMentor = () => {
       <MembersSectionHeader>Challenge Name</MembersSectionHeader>
       <InformationCardSection>
         {/* Example; title: challenge name, description: your name, onclick: go to new page with attempt*/}
-        <Card title="Meet landing page" description="your name" />
+        <Card title="3-Column Preview Card" description="Maxim and Wei Pin" />
       </InformationCardSection>
     </MembersSection>
   )
 }
-
 export default FrontendMentor
