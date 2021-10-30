ecce4ea4f3a7dd8911644d9fef3137d4b52c2105
shaokiat <shaokiat@gmail.com>
2021-10-31 01:05:51 +0800

    Add toggle for theme change

--- src/pages/FrontendMentor/skDashboard/index.tsx
+++ src/pages/FrontendMentor/skDashboard/index.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { DashboardHeader, FollowersCount, MainPage } from './skDashboard.styled'
+import { Switch } from 'antd'
+import React, { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { DashboardHeader, FollowersCount, MainPage, GlobalStyles, lightTheme, darkTheme } from './skDashboard.styled'
 
 const skDashboard = () => {
+  const [theme, setTheme] = useState('light')
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   return (
-    <MainPage>
-      <DashboardHeader>
-        Social Media Dashboard
-        <FollowersCount>Total Followers: 23,000</FollowersCount>
-      </DashboardHeader>
-      <hr
-        style={{
-          width: '100%',
-        }}
-      />
-    </MainPage>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <MainPage>
+        <DashboardHeader>
+          Social Media Dashboard
+          <FollowersCount>Total Followers: 23,000</FollowersCount>
+        </DashboardHeader>
+        <hr
+          style={{
+            width: '100%',
+          }}
+        />
+        <Switch onClick={themeToggler} />
+      </MainPage>
+    </ThemeProvider>
   )
 }
 
--- src/pages/FrontendMentor/skDashboard/skDashboard.styled.ts
+++ src/pages/FrontendMentor/skDashboard/skDashboard.styled.ts
@@ -1,4 +1,38 @@
 import styled from 'styled-components'
+import { createGlobalStyle, DefaultTheme } from 'styled-components'
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    body: string
+    text: string
+    toggleBorder: string
+    background: string
+  }
+}
+
+export const darkTheme: DefaultTheme = {
+  body: '#363537',
+  text: '#FAFAFA',
+  toggleBorder: '#6B8096',
+  background: '#999',
+}
+
+export const lightTheme: DefaultTheme = {
+  body: '#FFF',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  background: '#363537',
+}
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+    transition: all 0.50s linear;
+  }
+  `
 
 export const MainPage = styled.div`
   display: flex;
