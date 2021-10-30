b141a22b9f3595d6004b35e7effcb764625d148d
Chen Yiyang <elvachen2000@gmail.com>
2021-10-30 16:58:01 +0800

    Basic layout added, needs improvements

--- src/pages/FrontendMentor/ElvaYBPractice/index.tsx
+++ src/pages/FrontendMentor/ElvaYBPractice/index.tsx
@@ -1,10 +1,21 @@
+import { Content } from 'antd/lib/layout/layout'
 import React from 'react'
-import { MainPage, Header1 } from './styles/SinglePriceGridComponent.styled'
+import { MainPage, MainText, Header1, Text1, Text2, Body1, Header2 } from './styles/SinglePriceGridComponent.styled'
 
 const ElvaYBPractice = () => {
   return (
     <MainPage>
-      <Header1>Join our community</Header1>
+      <MainText>
+        <Header1>Join our community</Header1>
+        <Text1>30-day, hassle-free money back guarantee</Text1>
+        <Text2>
+          Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who
+          are serious about honing their skills.
+        </Text2>
+        <Body1>
+          <Header2>Monthly subscription</Header2>
+        </Body1>
+      </MainText>
     </MainPage>
   )
 }
--- src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
+++ src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
@@ -1,8 +1,37 @@
+import { Content } from 'antd/lib/layout/layout'
 import styled from 'styled-components'
+import { preProcessFile } from 'typescript'
 
 export const MainPage = styled.div`
+  height: 100%;
+  width: 100%;
+`
+
+export const MainText = styled.div`
   width: 1440px;
 `
+
 export const Header1 = styled.h1`
   color: hsl(179, 62%, 43%);
+  margin 50px 100px;
+`
+
+export const Text1 = styled.body`
+  color: hsl(71, 73%, 54%);
+  font-size: 16px;
+  margin: 0 100px;
+`
+export const Text2 = styled.body`
+  color: hsl(204, 43%, 93%);
+  font-size: 16px;
+  margin: 0 100px;
+  background-color: black;
+`
+export const Body1 = styled.div`
+  background-color: hsl(179, 62%, 43%);
+`
+export const Header2 = styled.h2`
+  color: white;
+  font-size: 16px;
+  margin: 50px 100px;
 `
