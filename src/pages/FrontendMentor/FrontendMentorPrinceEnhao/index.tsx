1b4b520550a3095a1512c023c0868ee341adffb0
Magmanat <princemjp@gmail.com>
2021-11-01 22:27:07 +0800

    added profile card and background

--- src/pages/FrontendMentor/FrontendMentorPrinceEnhao/index.tsx
+++ src/pages/FrontendMentor/FrontendMentorPrinceEnhao/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {
+  BottomBackground,
   MainContainer,
-  StyledHeader,
+  ProfileCardContainer,
   StyledImg,
-  StyledParagraph,
+  ProfileCardBackground,
   StyledSubheader,
+  TopBackground,
 } from './styles/FrontendMentorPrinceEnhao.styled'
 import BackgroundBottom from '../../../assets/princeenhao/bg-pattern-bottom.svg'
 import BackgroundCard from '../../../assets/princeenhao/bg-pattern-card.svg'
@@ -12,20 +14,11 @@ import BackgroundTop from '../../../assets/princeenhao/bg-pattern-top.svg'
 const FrontendMentorPrinceEnhao = () => {
   return (
     <MainContainer>
-      <StyledHeader>Welcome to the website</StyledHeader>
-      <StyledImg src={BackgroundTop} />
-      <StyledParagraph>Description...</StyledParagraph>
-      <br />
-      <StyledSubheader>Directions</StyledSubheader>
-      <StyledParagraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a ipsum lacinia, aliquam lectus sit amet, ultrices
-        tellus. Fusce posuere risus est, eget tempus metus iaculis posuere. Quisque dui velit, scelerisque non lectus
-        ut, mollis vulputate sapien. Curabitur sed mi tincidunt, convallis leo eu, iaculis ex. Aenean nec dui dolor.
-        Mauris facilisis faucibus odio et tempor. Nam consequat lorem eu gravida sollicitudin. Phasellus facilisis
-        iaculis neque eget luctus. Sed commodo dui sit amet viverra gravida. Cras sed lectus nisl.
-        <br />
-        <em>Come Join Us!</em>
-      </StyledParagraph>
+      <ProfileCardContainer>
+        <ProfileCardBackground src={BackgroundCard} />
+      </ProfileCardContainer>
+      <TopBackground src={BackgroundTop} />
+      <BottomBackground src={BackgroundBottom} />
     </MainContainer>
   )
 }
--- src/pages/FrontendMentor/FrontendMentorPrinceEnhao/styles/FrontendMentorPrinceEnhao.styled.ts
+++ src/pages/FrontendMentor/FrontendMentorPrinceEnhao/styles/FrontendMentorPrinceEnhao.styled.ts
@@ -1,25 +1,56 @@
 import styled from 'styled-components'
-import BackgroundBottom from '../../../assets/princeenhao/bg-pattern-bottom.svg'
-import BackgroundCard from '../../../assets/princeenhao/bg-pattern-card.svg'
-import img from '../../../assets/princeenhao/bg-pattern-top.svg'
+// import BackgroundBottom from '../../../assets/princeenhao/bg-pattern-bottom.svg'
+// import BackgroundCard from '../../../assets/princeenhao/bg-pattern-card.svg'
+// import img from '../../../assets/princeenhao/bg-pattern-top.svg'
 
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 1rem 3rem;
-  background-image: url(${img});
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  background-color: #19a2ae;
+  color: #19a2ae;
+`
+export const TopBackground = styled.img`
+  z-index: 1;
+  height: auto;
+  width: 50vw;
+  left: 0vw;
+  top: -40vh;
+  position: fixed;
+`
+export const BottomBackground = styled.img`
+  z-index: 1;
+  height: auto;
+  width: 50vw;
+  right: 0px;
+  bottom: -40vh;
+  position: fixed;
+`
+export const ProfileCardContainer = styled.div`
+  z-index: 2;
+  height: 60vh;
+  width: 55vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  position: relative;
+  background-color: white;
+  opacity: 1;
+  border: 0.3vh solid hsl(229, 23%, 23%);
+  box-sizing: border-box;
+  box-shadow: 0.5vh 0.5vh 0.5vh 0.5vh rgba(0, 0, 0, 0.08);
+  border-radius: 2vh;
+  overflow: hidden;
 `
 
-export const StyledHeader = styled.h1`
-  color: rgb(25, 25, 121);
-  font-family: Arial, sans-serif;
-  margin: 1rem auto;
-`
-
-export const StyledParagraph = styled.p`
-  text-align: justify;
+export const ProfileCardBackground = styled.img`
+  overflow: hidden;
+  width: 100%;
+  height: 20%;
 `
 
 export const StyledSubheader = styled.h2`
