d8da9e9bbc8f10088fb8b4c994a52fc76ee0eff9
Maxim Yam <maximyam@gmail.com>
2021-11-01 20:51:20 +0800

    added color text and structure

--- src/pages/FrontendMentor/maximwp_website/index.tsx
+++ src/pages/FrontendMentor/maximwp_website/index.tsx
@@ -1,51 +1,41 @@
 import React from 'react'
-
+import {
+  FullScreenContainer,
+  StyledHeader,
+  StyledParagraph,
+  StyledSubheader,
+  StyledImg,
+  MainContainer1,
+  MainContainer2,
+  MainContainer3,
+} from './styles/styled'
 import styled from 'styled-components'
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin: 1rem 3rem;
-`
-
-const StyledHeader = styled.h1`
-  color: rgb(25, 25, 121);
-  font-family: Arial, sans-serif;
-  margin: 1rem auto;
-`
-
-const StyledParagraph = styled.p`
-  text-align: justify;
-`
-
-const StyledSubheader = styled.h2`
-  margin: 1rem 0 0 0;
-  font-family: Arial, Helvetica, sans-serif;
-`
-
-const StyledImg = styled.img`
-  border-radius: 10px;
-`
 
 const Lesson1Example = () => {
   return (
-    <MainContainer>
-      <StyledHeader>Welcome to the website</StyledHeader>
-      <StyledImg src="https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png" alt="random" />
-      <StyledParagraph>Description...</StyledParagraph>
-      <br />
-      <StyledSubheader>Directions</StyledSubheader>
-      <StyledParagraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a ipsum lacinia, aliquam lectus sit amet, ultrices
-        tellus. Fusce posuere risus est, eget tempus metus iaculis posuere. Quisque dui velit, scelerisque non lectus
-        ut, mollis vulputate sapien. Curabitur sed mi tincidunt, convallis leo eu, iaculis ex. Aenean nec dui dolor.
-        Mauris facilisis faucibus odio et tempor. Nam consequat lorem eu gravida sollicitudin. Phasellus facilisis
-        iaculis neque eget luctus. Sed commodo dui sit amet viverra gravida. Cras sed lectus nisl.
-        <br />
-        <em>Come Join Us!</em>
-      </StyledParagraph>
-    </MainContainer>
+    <FullScreenContainer>
+      <MainContainer1>
+        <StyledHeader>Sedans</StyledHeader>
+        <StyledSubheader>
+          Choose a sedan for its affordability and excellent fuel economy. Ideal for curising in the city or on your
+          next road trip
+        </StyledSubheader>
+      </MainContainer1>
+      <MainContainer2>
+        <StyledHeader>SUVs</StyledHeader>
+        <StyledSubheader>
+          Take an SUV for its spacious interior, power, and verstality. Perfect for your next family vacation and
+          off-road adventures
+        </StyledSubheader>
+      </MainContainer2>
+      <MainContainer3>
+        <StyledHeader> Luxury </StyledHeader>
+        <StyledSubheader>
+          Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and
+          arrive in a style.
+        </StyledSubheader>
+      </MainContainer3>
+    </FullScreenContainer>
   )
 }
 
---
+++ src/pages/FrontendMentor/maximwp_website/styles/styled.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+import '@fontsource/lexend-deca'
+
+export const FullScreenContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  position: relative;
+  width: 100%;
+  height: 100%;
+`
+export const MainContainer1 = styled.div`
+  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  font-family: 'Lexend Deca';
+  background-color: rgb(238, 183, 124);
+  border-radius: 5px;
+`
+export const MainContainer2 = styled.div`
+  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  font-family: 'Lexend Deca';
+  background-color: rgb(0, 105, 112);
+  border-radius: 5px;
+`
+export const MainContainer3 = styled.div`
+  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+  height: 100%;
+  font-family: 'Lexend Deca';
+  background-color: rgb(0, 66, 65);
+  border-radius: 5px;
+`
+export const StyledHeader = styled.h1`
+  font-family: 'Lexend Deca';
+  margin: 1rem auto;
+  text-align: center;
+  vertical-align: middle;
+`
+export const StyledParagraph = styled.p`
+  text-align: justify;
+`
+export const StyledSubheader = styled.h2`
+  margin: 1rem 0 0 0;
+  font-family: 'Lexend Deca';
+`
+export const StyledImg = styled.img`
+  border-radius: 10px;
+`
