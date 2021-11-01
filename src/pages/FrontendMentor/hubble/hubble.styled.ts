2eea4b94ac3b12626b9002eade6b71157df39fa8
Tai <kktai1512@outlook.com>
2021-11-02 00:10:42 +0800

    added lots of stuff

--- src/pages/FrontendMentor/hubble/hubble.styled.ts
+++ src/pages/FrontendMentor/hubble/hubble.styled.ts
@@ -4,8 +4,49 @@ import bgdesk from '../../../assets/hubble/bgdesk.svg'
 export const MainContainer = styled.div`
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
   background-image: url(${bgdesk});
   min-width: 1440px;
   min-height: 800px;
   background-color: hsl(257, 40%, 49%);
+  justify-content: center;
+  allgin-items: center;
+`
+export const LColumn = styled.div`
+  height: 100%;
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  allign-items: center;
+  justify-content: center:
+`
+export const LogoBox = styled.div`
+  height: 100%;
+  width: 100%;
+  margin: auto;
+`
+
+export const Rows = styled.div`
+  padding: 10px 50px;
+  display: flex;
+`
+
+export const HeaderText = styled.div`
+  padding: 20px 50px;
+  display: flex;
+  font-family: Poppins;
+  font-weight: 600;
+  font-size: 40px;
+  color: white;
+`
+
+export const BodyText = styled.div`
+  padding: 30px 50px;
+  display: flex;
+  font-family: Poppins;
+  font-weight: 400;
+  font-size: 25px;
+  color: white;
 `
--- src/pages/FrontendMentor/hubble/index.tsx
+++ src/pages/FrontendMentor/hubble/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import { MainContainer } from './hubble.styled'
+import { MainContainer, LColumn, LogoBox, Rows, HeaderText, BodyText } from './hubble.styled'
 //import HubbleCard from '../../../components/HubbleCard'
-import bgdesk from '../../../assets/hubble/bgdesk.svg'
+import comp from '../../../assets/hubble/illustration-mockups.svg'
+import logo from '../../../assets/hubble/logo.svg'
 
 const Hubble = () => {
   return (
     <>
-      <MainContainer>Hello</MainContainer>
+      <MainContainer>
+        <Rows>
+          <LogoBox>
+            <img src={logo} />
+          </LogoBox>
+        </Rows>
+        <Rows>
+          <LColumn>
+            <img src={comp} />
+          </LColumn>
+          <LColumn>
+            <HeaderText>Build the community your fans will love</HeaderText>
+            <BodyText>
+              Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create
+              connections with your users as you engage in genuine connection.
+            </BodyText>
+          </LColumn>
+        </Rows>
+      </MainContainer>
     </>
   )
 }
