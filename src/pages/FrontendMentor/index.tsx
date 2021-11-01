a6b5cf1ad823dd53247c07c8dbf8b50194fb00c0
Poh Wei Pin <firwer123@hotmail.com>
2021-11-01 20:10:04 +0800

    Fix index.tsx duplicate card

--- src/pages/FrontendMentor/index.tsx
+++ src/pages/FrontendMentor/index.tsx
@@ -33,7 +33,6 @@ const FrontendMentor = () => {
           onClick={() => history.push(PATHS.MAXIM_WEIPIN_WEBSITE)}
         />
         <Card title="Meet landing page" description="your name" />
-        <Card title="3-column preview card component challenge hub (Maxim and Wei Pin)" description="your name" />
       </InformationCardSection>
     </MembersSection>
   )
