741a32ecd0992fcfb4326c105a2493d2d111aea1
Wang Yongbin <yongbin_01@hotmail.com>
2021-11-01 16:04:57 +0800

    Updated background, created container components, text components, tried to align

--- src/pages/FrontendMentor/ElvaYBPractice/index.tsx
+++ src/pages/FrontendMentor/ElvaYBPractice/index.tsx
@@ -1,20 +1,33 @@
 import { Content } from 'antd/lib/layout/layout'
 import React from 'react'
-import { MainPage, MainText, Header1, Text1, Text2, Body1, Header2 } from './styles/SinglePriceGridComponent.styled'
+import {
+  MainPage,
+  MainText,
+  MiddleContainer,
+  JoinCommText1,
+  JoinCommText2,
+  JoinCommText3,
+  BottomLeftContainer,
+  Header2,
+  BottomRightContainer,
+} from './styles/SinglePriceGridComponent.styled'
 
 const ElvaYBPractice = () => {
   return (
     <MainPage>
       <MainText>
-        <Header1>Join our community</Header1>
-        <Text1>30-day, hassle-free money back guarantee</Text1>
-        <Text2>
-          Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who
-          are serious about honing their skills.
-        </Text2>
-        <Body1>
+        <MiddleContainer>
+          <JoinCommText1> Join our community</JoinCommText1>
+          <JoinCommText2>30-day, hassle-free money back guarantee</JoinCommText2>
+          <JoinCommText3>
+            Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who
+            are serious about honing their skills
+          </JoinCommText3>
+        </MiddleContainer>
+        <BottomLeftContainer>
           <Header2>Monthly subscription</Header2>
-        </Body1>
+        </BottomLeftContainer>
+        <BottomRightContainer>Why us </BottomRightContainer>
       </MainText>
     </MainPage>
   )
--- src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
+++ src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
@@ -3,35 +3,55 @@ import styled from 'styled-components'
 import { preProcessFile } from 'typescript'
 
 export const MainPage = styled.div`
-  height: 100%;
+  min-height: 100vh;
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(225, 217, 221, 0.8);
 `
 
 export const MainText = styled.div`
   width: 1440px;
 `
 
-export const Header1 = styled.h1`
+export const MiddleContainer = styled.div`
   color: hsl(179, 62%, 43%);
+  border-radius: 8px;
   margin 50px 100px;
+  box-shadow: 0px 0px 14px 1px #00000024;
+  background-color: #FFFFFF;
+  width: 70%;
+  height: 40vh;
 `
 
-export const Text1 = styled.body`
-  color: hsl(71, 73%, 54%);
+export const JoinCommText1 = styled.div`
+  color: 239593;
   font-size: 16px;
   margin: 0 100px;
 `
-export const Text2 = styled.body`
-  color: hsl(204, 43%, 93%);
+export const JoinCommText2 = styled.div`
+  color: #ded02b;
   font-size: 16px;
   margin: 0 100px;
-  background-color: black;
 `
-export const Body1 = styled.div`
-  background-color: hsl(179, 62%, 43%);
+export const JoinCommText3 = styled.div`
+  color: #9b9a8e;
+  font-size: 16px;
+  margin: 0 100px;
 `
-export const Header2 = styled.h2`
+export const BottomLeftContainer = styled.div`
+  background-color: #2db3a3;
+  width: 40%;
+  height: 40vh;
+`
+export const Header2 = styled.div`
   color: white;
   font-size: 16px;
   margin: 50px 100px;
+`
+export const BottomRightContainer = styled.div`
+  background-color: #38e0cd;
+  width: 40%;
+  height: 40vh;
 `
