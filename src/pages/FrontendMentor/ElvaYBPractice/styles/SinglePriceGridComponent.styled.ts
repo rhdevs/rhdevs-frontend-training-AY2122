5f15ebf8a5a909880ee8f9955e352b3f776b14fa
Wang Yongbin <yongbin_01@hotmail.com>
2021-11-04 22:49:25 +0800

    Added top container media and responsive styling

--- src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
+++ src/pages/FrontendMentor/ElvaYBPractice/styles/SinglePriceGridComponent.styled.ts
@@ -17,6 +17,9 @@ export const MainContainer = styled.div`
 export const BottomContainer = styled.div`
   display: flex;
   flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `
 
 export const MiddleContainer = styled.div`
@@ -24,29 +27,52 @@ export const MiddleContainer = styled.div`
   background-color: #ffffff;
   width: 100%;
   height: 35vh;
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    padding: 10%;
+    height: 60vw;
+  }
 `
 
 export const JoinCommText1 = styled.div`
   color: hsl(179, 62%, 43%);
   font-size: 30px;
   font-weight: bold;
-  padding: 30px 0px 10px 80px;
+  padding: 30px 00px 10px 80px;
+  @media (max-width: 768px) {
+    font-size: 5vw;
+    padding: 2vw;
+  }
 `
 export const JoinCommText2 = styled.div`
   color: hsl(71, 73%, 54%);
   font-size: 16px;
   font-weight: bold;
-  margin: 0px 0px 10px 80px;
+  padding: 00px 00px 10px 80px;
+  @media (max-width: 768px) {
+    font-size: 3vw;
+    padding: 2vw;
+  }
 `
 export const JoinCommText3 = styled.div`
   color: hsl(218, 22%, 67%);
   font-size: 16px;
-  margin: 0px 0px 0px 80px;
+  padding: 00px 00px 00px 80px;
+  break-between: always;
+  @media (max-width: 768px) {
+    font-size: 2.5vw;
+    padding: 2vw;
+    break-between: none;
+  }
 `
 export const BottomLeftContainer = styled.div`
   background-color: hsl(179, 62%, 43%);
   width: 50%;
   height: 50vh;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `
 export const BottomLeftMonthly = styled.div`
   color: hsl(204, 43%, 93%);
@@ -80,6 +106,9 @@ export const BottomRightContainer = styled.div`
   background-color: hsl(179, 62%, 48%);
   width: 50%;
   height: 50vh;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `
 export const BottomRightShort = styled.div`
   color: hsl(204, 43%, 93%);
