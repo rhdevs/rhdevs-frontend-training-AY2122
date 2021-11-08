7c97cbbe704a6a8c5bf2428a579adcfc632b512d
Poh Wei Pin <firwer123@hotmail.com>
2021-11-09 01:47:56 +0800

    Styling for the websites, background svg added in

--- src/pages/FrontendMentor/Maximwp_proj2/index.tsx
+++ src/pages/FrontendMentor/Maximwp_proj2/index.tsx
@@ -2,6 +2,7 @@ import { Button, Switch } from 'antd'
 import React, { useState, useCallback } from 'react'
 import {
   MainContainer,
+  MainBox,
   TopBox,
   Minibox,
   SideCont,
@@ -13,6 +14,7 @@ import {
   Detailsincont,
   ButtonStyle,
   ButtonStyle2,
+  SwitchStyle,
 } from './styles/styled'
 
 export default function App() {
@@ -20,46 +22,48 @@ export default function App() {
 
   return (
     <>
-      <TopBox>
-        <div className="App">
-          <Pricing> Our Pricing </Pricing>
-          <Minibox>
-            <ToggleBetween> Annually </ToggleBetween>
-            <Switch onChange={() => setMonthly(!monthly)}></Switch>
-            <ToggleBetween> Monthly </ToggleBetween>
-          </Minibox>
-        </div>
-      </TopBox>
-      <MainContainer>
-        <SideCont>
-          <StyledHeader>Basic</StyledHeader>
-          {monthly ? <StyledNumber> $19.99 </StyledNumber> : <StyledNumber> $199.99 </StyledNumber>}
-          <Detailsincont>500 GB Storage</Detailsincont>
-          <Detailsincont>2 Users Allowed</Detailsincont>
-          <Detailsincont>Send up to 3 GB</Detailsincont>
-          <Button style={ButtonStyle2}> Learn more </Button>
-        </SideCont>
-        <CentreCont>
-          <StyledHeader typ={'Main'}>Professional</StyledHeader>
-          {monthly ? (
-            <StyledNumber typ={'Main'}> $24.99 </StyledNumber>
-          ) : (
-            <StyledNumber typ={'Main'}> $249.99 </StyledNumber>
-          )}
-          <Detailsincont typ={'Main'}>1 TB Storage</Detailsincont>
-          <Detailsincont typ={'Main'}>5 Users Allowed</Detailsincont>
-          <Detailsincont typ={'Main'}>Send up to 10 GB</Detailsincont>
-          <Button style={ButtonStyle}> Learn more </Button>
-        </CentreCont>
-        <SideCont>
-          <StyledHeader>Master</StyledHeader>
-          {monthly ? <StyledNumber> $39.99 </StyledNumber> : <StyledNumber> $359.99 </StyledNumber>}
-          <Detailsincont>2 TB Storage</Detailsincont>
-          <Detailsincont>10 Users Allowed</Detailsincont>
-          <Detailsincont>Send up to 20 GB</Detailsincont>
-          <Button style={ButtonStyle2}> Learn more </Button>
-        </SideCont>
-      </MainContainer>
+      <MainBox>
+        <TopBox>
+          <div className="App">
+            <Pricing> Our Pricing </Pricing>
+            <Minibox>
+              <ToggleBetween> Annually </ToggleBetween>
+              <Switch style={SwitchStyle} onChange={() => setMonthly(!monthly)}></Switch>
+              <ToggleBetween> Monthly </ToggleBetween>
+            </Minibox>
+          </div>
+        </TopBox>
+        <MainContainer>
+          <SideCont>
+            <StyledHeader>Basic</StyledHeader>
+            {monthly ? <StyledNumber> $19.99 </StyledNumber> : <StyledNumber> $199.99 </StyledNumber>}
+            <Detailsincont>500 GB Storage</Detailsincont>
+            <Detailsincont>2 Users Allowed</Detailsincont>
+            <Detailsincont>Send up to 3 GB</Detailsincont>
+            <Button style={ButtonStyle2}> Learn more </Button>
+          </SideCont>
+          <CentreCont>
+            <StyledHeader typ={'Main'}>Professional</StyledHeader>
+            {monthly ? (
+              <StyledNumber typ={'Main'}> $24.99 </StyledNumber>
+            ) : (
+              <StyledNumber typ={'Main'}> $249.99 </StyledNumber>
+            )}
+            <Detailsincont typ={'Main'}>1 TB Storage</Detailsincont>
+            <Detailsincont typ={'Main'}>5 Users Allowed</Detailsincont>
+            <Detailsincont typ={'Main'}>Send up to 10 GB</Detailsincont>
+            <Button style={ButtonStyle}> Learn more </Button>
+          </CentreCont>
+          <SideCont>
+            <StyledHeader>Master</StyledHeader>
+            {monthly ? <StyledNumber> $39.99 </StyledNumber> : <StyledNumber> $359.99 </StyledNumber>}
+            <Detailsincont>2 TB Storage</Detailsincont>
+            <Detailsincont>10 Users Allowed</Detailsincont>
+            <Detailsincont>Send up to 20 GB</Detailsincont>
+            <Button style={ButtonStyle2}> Learn more </Button>
+          </SideCont>
+        </MainContainer>
+      </MainBox>
     </>
   )
 }
--- src/pages/FrontendMentor/Maximwp_proj2/styles/styled.ts
+++ src/pages/FrontendMentor/Maximwp_proj2/styles/styled.ts
@@ -2,6 +2,14 @@ import styled from 'styled-components'
 import bgbtm from '../images/bg-bottom.svg'
 import bgtop from '../images/bg-top.svg'
 
+export const MainBox = styled.div`
+  width: 100%;
+  background-color: hsl(228, 100%, 98%);
+  background: url(${bgbtm}), url(${bgtop});
+  background-repeat: no-repeat;
+  background-position: left bottom, right top;
+`
+
 export const TopBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -21,6 +29,8 @@ export const Pricing = styled.div`
   text-align: center;
   margin: 1rem;
   font-size: 40px;
+  font-weight: 700;
+  color: rgb(110, 114, 143);
 `
 export const ToggleBetween = styled.div`
   text-align: center;
@@ -39,15 +49,16 @@ export const MainContainer = styled.div`
     flex-direction: column;
   }
 `
+//  #background-color: hsl(234, 14%, 74%);
 export const CentreCont = styled.div`
-  height: 600px;
+  height: 650px;
   width: 400px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: hsl(234, 14%, 74%);
+  background-image: linear-gradient(to right, hsl(236, 72%, 79%), hsl(237, 63%, 64%));
   border-width: 5px;
-  border-radius: 5px;
+  border-radius: 10px;
 `
 export const SideCont = styled.div`
   height: 600px;
@@ -55,7 +66,7 @@ export const SideCont = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: hsl(240, 78%, 98%);
+  background-color: hsl(0, 0%, 100%);
   border-width: 5px;
   border-radius: 5px;
 `
@@ -65,16 +76,16 @@ export const StyledHeader = styled.p<{ typ?: string }>`
   font-weight: 700;
   vertical-align: middle;
   text-align: center;
-  ${(props) => (props.typ === 'Main' ? 'color: hsl(240, 78%, 98%);' : 'color: hsl(234, 14%, 74%);')}
+  ${(props) => (props.typ === 'Main' ? 'color: hsl(240, 78%, 98%);' : 'hsl(233, 13%, 49%);')}
     }
 `
 export const StyledNumber = styled.p<{ typ?: string }>`
   margin: 2rem;
-  font-size: 52px;
+  font-size: 80px;
   font-weight: 700;
   vertical-align: middle;
   text-align: center;
-  ${(props) => (props.typ === 'Main' ? 'color: hsl(240, 78%, 98%);' : 'color: hsl(234, 14%, 74%);')}
+  ${(props) => (props.typ === 'Main' ? 'color: hsl(240, 78%, 98%);' : 'color: rgb(74, 76, 97);')}
 }
 `
 export const Detailsincont = styled.p<{ typ?: string }>`
@@ -86,21 +97,20 @@ export const Detailsincont = styled.p<{ typ?: string }>`
   ${(props) => (props.typ === 'Main' ? 'color: hsl(240, 78%, 98%);' : 'color: hsl(234, 14%, 74%);')}
 `
 export const SwitchStyle = {
-  width: '150px',
-  height: '45px',
+  width: '60px',
+  height: '35px',
   borderRadius: 100,
-  backgroundColor: '#fff',
+  backgroundImage: 'linear-gradient(to right, hsl(236, 72%, 79%), hsl(237, 63%, 64%))',
   color: '#016972',
   alignItems: 'center',
-  margin: '10%',
   'font-size': '15px',
 }
 export const ButtonStyle = {
-  width: '150px',
+  width: '300px',
   height: '45px',
-  borderRadius: 100,
-  backgroundColor: 'hsl(240, 78%, 98%)',
-  color: '#016972',
+  borderRadius: 180,
+  backgroundColor: 'hsl(0, 0%, 100%)',
+  color: '#000',
   'vertical-align': 'middle',
   alignItems: 'center',
   margin: 'auto',
@@ -108,11 +118,12 @@ export const ButtonStyle = {
   'border-radius': '2px',
 }
 export const ButtonStyle2 = {
-  width: '150px',
+  width: '300px',
   height: '45px',
   borderRadius: 100,
   backgroundColor: 'hsl(234, 14%, 74%)',
-  color: '#016972',
+  backgroundImage: 'linear-gradient(to right, hsl(236, 72%, 79%), hsl(237, 63%, 64%))',
+  color: '#fff',
   'vertical-align': 'middle',
   alignItems: 'center',
   margin: 'auto',
