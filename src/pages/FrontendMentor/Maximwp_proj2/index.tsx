3092456b563f65475ff240f1b4c0c91b04f7a348
Maxim Yam <maximyam@gmail.com>
2021-11-08 23:58:53 +0800

    foundation of website, created hook to change number, to add in more css

--- src/pages/FrontendMentor/Maximwp_proj2/index.tsx
+++ src/pages/FrontendMentor/Maximwp_proj2/index.tsx
@@ -1,51 +1,56 @@
-import React from 'react'
+import { Button, Switch } from 'antd'
+import React, { useState, useCallback } from 'react'
 import {
   MainContainer,
+  TopBox,
+  Minibox,
+  SideCont,
+  CentreCont,
   StyledHeader,
-  StyledParagraph,
-  CardContainer,
-  StyledLogo,
-  RoundButton1,
-  RoundButton2,
-  RoundButton3,
+  Pricing,
+  StyledNumber,
+  ToggleBetween,
+  Detailsincont,
 } from './styles/styled'
-import { Button } from 'antd'
 
-const Lesson1Example = () => {
+export default function App() {
+  const [monthly, setMonthly] = useState(false)
+
   return (
-    <MainContainer>
-      <CardContainer typ={'Sedan'}>
-        <StyledHeader>Sedans</StyledHeader>
-        <StyledParagraph>
-          Choose a sedan for its testy and excellent fuel economy. Ideal for curising in the city or on your next road
-          trip
-        </StyledParagraph>
-        <Button type="primary" style={RoundButton1}>
-          Learn more
-        </Button>
-      </CardContainer>
-      <CardContainer typ={'SUV'}>
-        <StyledHeader>SUVs</StyledHeader>
-        <StyledParagraph>
-          Take an SUV for its spacious interior, power, and verstality. Perfect for your family vacation and off-road
-          adventures.
-        </StyledParagraph>
-        <Button type="primary" style={RoundButton2}>
-          Learn more
-        </Button>
-      </CardContainer>
-      <CardContainer>
-        <StyledHeader> Luxury </StyledHeader>
-        <StyledParagraph>
-          Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and
-          arrive in a style.
-        </StyledParagraph>
-        <Button type="primary" style={RoundButton3}>
-          Learn more
-        </Button>
-      </CardContainer>
-    </MainContainer>
+    <>
+      <TopBox>
+        <div className="App">
+          <Pricing> Our Pricing </Pricing>
+          <Minibox>
+            <ToggleBetween> Annually </ToggleBetween>
+            <Switch onChange={() => setMonthly(!monthly)}></Switch>
+            <ToggleBetween> Monthly </ToggleBetween>
+          </Minibox>
+        </div>
+      </TopBox>
+      <MainContainer>
+        <SideCont>
+          <StyledHeader>Basic</StyledHeader>
+          {monthly ? <StyledNumber> $19.99 </StyledNumber> : <StyledNumber> $199.99 </StyledNumber>}
+          <Detailsincont>500 GB Storage</Detailsincont>
+          <Detailsincont>2 Users Allowed</Detailsincont>
+          <Detailsincont>Send up to 3 GB</Detailsincont>
+        </SideCont>
+        <CentreCont>
+          <StyledHeader>Professional</StyledHeader>
+          {monthly ? <StyledNumber> $24.99 </StyledNumber> : <StyledNumber> $249.99 </StyledNumber>}
+          <Detailsincont>1 TB Storage</Detailsincont>
+          <Detailsincont>5 Users Allowed</Detailsincont>
+          <Detailsincont>Send up to 10 GB</Detailsincont>
+        </CentreCont>
+        <SideCont>
+          <StyledHeader>Master</StyledHeader>
+          {monthly ? <StyledNumber> $39.99 </StyledNumber> : <StyledNumber> $359.99 </StyledNumber>}
+          <Detailsincont>2 TB Storage</Detailsincont>
+          <Detailsincont>10 Users Allowed</Detailsincont>
+          <Detailsincont>Send up to 20 GB</Detailsincont>
+        </SideCont>
+      </MainContainer>
+    </>
   )
 }
-
-export default Lesson1Example
--- src/pages/FrontendMentor/Maximwp_proj2/styles/styled.ts
+++ src/pages/FrontendMentor/Maximwp_proj2/styles/styled.ts
@@ -2,82 +2,80 @@ import styled from 'styled-components'
 import bgbtm from '../images/bg-bottom.svg'
 import bgtop from '../images/bg-top.svg'
 
+export const TopBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  padding-top: 40px;
+  padding-bottom: 10px;
+`
+export const Minibox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin: 1rem;
+  font-size: 30px;
+`
+export const Pricing = styled.div`
+  text-align: center;
+  margin: 1rem;
+  font-size: 40px;
+`
+export const ToggleBetween = styled.div`
+  text-align: center;
+  font-size: 12px;
+`
 export const MainContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: safe center;
   position: relative;
-  width: 70%;
-  left: 15%;
-  padding-top: 100px;
+  padding-top: 50px;
   padding-bottom: 100px;
   background-image: url(${bgbtm}) bottom left, url(${bgtop}) top right;
-  @media (max-width: 1024px) {
-    display: flex;
-    flex-direction: column;
-  }
 `
-export const CardContainer = styled.div<{ typ?: string }>`
+export const CentreCont = styled.div`
   height: 600px;
+  width: 300px;
   flex-direction: row;
   align-items: center;
-  font-family: 'Lexend Deca';
-  background-color: rgb(226, 133, 37);
-  ${(props) =>
-    props.typ === 'Sedan'
-      ? 'background-color: rgb(226, 133, 37); border-radius: 10px 0px 0px 10px;'
-      : props.typ === 'SUV'
-      ? 'background-color: rgb(1, 105, 114);'
-      : 'background-color: rgb(0, 64, 63); border-radius: 0px 10px 10px 0px;'}
+  background-color: hsl(234, 14%, 74%);
+  border-width: 5px;
+  border-radius: 5px;
+`
+export const SideCont = styled.div`
+  height: 600px;
+  width: 300px;
+  flex-direction: row;
+  align-items: center;
+  background-color: hsl(240, 78%, 98%);
+  border-width: 5px;
+  border-radius: 5px;
 `
 export const StyledHeader = styled.p`
-  font-family: 'Lexend Deca';
   margin: 2rem;
-  font-size: 36px;
+  font-size: 24px;
+  font-weight: 700;
   vertical-align: middle;
-  color: #ffffff;
+  text-align: center;
 `
-export const StyledParagraph = styled.p`
-  text-align: justify;
+export const StyledNumber = styled.p`
   margin: 2rem;
-  font-family: 'Lexend Deca';
-  font-size: 15px;
-  color: #ffffff;
-  line-height: 2.5;
+  font-size: 52px;
+  font-weight: 700;
+  vertical-align: middle;
+  text-align: center;
 `
-export const StyledLogo = styled.img`
-  width: 100px;
+export const Detailsincont = styled.p`
   margin: 2rem;
-  padding: 5px;
-  position: relative;
+  font-size: 24px;
+  font-weight: 500;
+  vertical-align: middle;
+  text-align: center;
 `
-export const RoundButton1 = {
-  width: '150px',
-  height: '45px',
-  borderRadius: 100,
-  backgroundColor: '#fff',
-  color: '#e28525',
-  alignItems: 'center',
-  margin: '10%',
-  'font-size': '15px',
-}
-export const RoundButton2 = {
-  width: '150px',
-  height: '45px',
-  borderRadius: 100,
-  backgroundColor: '#fff',
-  color: '#016972',
-  alignItems: 'center',
-  margin: '10%',
-  'font-size': '15px',
-}
-export const RoundButton3 = {
-  width: '150px',
-  height: '45px',
-  borderRadius: 100,
-  backgroundColor: '#fff',
-  color: '#00403f',
+export const Switch = {
   alignItems: 'center',
   margin: '10%',
   'font-size': '15px',
