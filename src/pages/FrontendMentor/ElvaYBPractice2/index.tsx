ecbaaedfa030c3cfb2915e4c02f3fca1ab75405f
Wang Yongbin <yongbin_01@hotmail.com>
2021-11-10 22:04:48 +0800

    Createed components and slider with steps

--- src/pages/FrontendMentor/ElvaYBPractice2/index.tsx
+++ src/pages/FrontendMentor/ElvaYBPractice2/index.tsx
@@ -1,18 +1,50 @@
-import Slider from '@ant-design/react-slick'
 import React from 'react'
+import { Slider, Switch } from 'antd'
 
-import { MainPage, TopComponent, MainComponent, SliderBar, Slidercontainer } from './styles/SliderComponent.styled'
+import {
+  MainPage,
+  TopComponent,
+  TopComponentTopText,
+  TopComponentBottomText,
+  MainComponent,
+  SliderBar,
+  Slidercontainer,
+  PageViewPriceContainer,
+  PageViewContainer,
+  PriceContainer,
+} from './styles/SliderComponent.styled'
+
+const marks = {
+  0: '0°C',
+  26: '26°C',
+  37: '37°C',
+  100: {
+    style: {
+      color: '#f50',
+    },
+    label: <strong>100°C</strong>,
+  },
+}
 
 const ElvaYBPractice = () => {
   return (
-    <MainPage>
-      <TopComponent>Simple, Traffic Based Pricing</TopComponent>
-      <MainComponent>
-        <Slidercontainer>
-          <Slider />
-        </Slidercontainer>
-      </MainComponent>
-    </MainPage>
+    <>
+      <MainPage>
+        <TopComponent>
+          <TopComponentTopText>Simple, traffic-based Pricing</TopComponentTopText>
+          <TopComponentBottomText>Sign-up for our 30 day trial. No credit card required.</TopComponentBottomText>
+        </TopComponent>
+        <MainComponent>
+          <PageViewPriceContainer>
+            <PageViewContainer>100k PageViews</PageViewContainer>
+            <PriceContainer>$16.00/Month</PriceContainer>
+          </PageViewPriceContainer>
+          <Slidercontainer>
+            <Slider marks={marks} step={null} defaultValue={37} />
+          </Slidercontainer>
+        </MainComponent>
+      </MainPage>
+    </>
   )
 }
 
--- src/pages/FrontendMentor/ElvaYBPractice2/styles/SliderComponent.styled.ts
+++ src/pages/FrontendMentor/ElvaYBPractice2/styles/SliderComponent.styled.ts
@@ -5,14 +5,17 @@ import BackgroundPic from '../../../../assets/ElvaYongbin/BackgroundPic.svg'
 import PatternCirclePic from '../../../../assets/ElvaYongbin/images/pattern-circles.svg'
 
 export const MainPage = styled.div`
+  z-index: 0;
   min-height: 100vh;
-  width: 100%;
+  width: 100vw;
   display: flex;
   align-items: center;
+  flex-direction: column;
   justify-content: center;
   background-image: url(${BackgroundPic});
   background-repeat: no-repeat;
   background-size: 100% 50%;
+  color: hsl(230, 100%, 99%);
 `
 
 export const TopComponent = styled.div`
@@ -22,19 +25,33 @@ export const TopComponent = styled.div`
   padding: 10px;
   width: 40%;
   text-align: center;
-  height: 50%;
-  margin: 0 auto;
+  position: relative;
+`
+export const TopComponentTopText = styled.div`
+  color: black;
+  font-size: 15px;
+`
+export const TopComponentBottomText = styled.div`
+  color: grey;
+  font-size: 7px;
 `
 export const MainComponent = styled.div`
+  z-index: 2;
   border-radius: 15px;
-  width: 70%;
-  background-color: red;
-  height: 70%;
+  width: 70vw;
+  height: 70vh;
+  display: flex;
+  position: relative;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `
 
 export const SliderBoxComponent = styled.div``
 export const Slidercontainer = styled.div`
-  width: 100%;
+  width: 60%;
+  align-items: center;
 `
 
 export const SliderBar = styled.div`
@@ -49,5 +66,18 @@ export const SliderBar = styled.div`
   transition: opacity 0.2s;
 `
 export const MainBottomComponent = styled.div``
+export const PageViewPriceContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`
+export const PageViewContainer = styled.div`
+  width: 50%;
+  color: black;
+`
+export const PriceContainer = styled.div`
+  width: 50%;
+  color: black;
+`
 export const RightTrialButton = styled.div``
 export const LeftTrialText = styled.div``
