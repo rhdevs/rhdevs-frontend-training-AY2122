5ec441aebea05212dcb1d37ef0af67f643846882
shaokiat <shaokiat@gmail.com>
2021-11-11 01:28:21 +0800

    Update line height

--- src/components/SkIPAddress/styles/ContentBox.styled.ts
+++ src/components/SkIPAddress/styles/ContentBox.styled.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 export const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 7vh auto 0 2vw;
+  margin: 5vh auto 0 2vw;
   width: 13vw;
   height: 100%;
   flex-wrap: wrap;
-  overflow-y: auto;
+  overflow: auto;
   @media (max-width: 786px) {
     width: 100%;
     margin: 1.5vh 0 1vh 0;
@@ -27,10 +27,9 @@ export const StyledHeader = styled.div`
 `
 
 export const StyledBody = styled.div`
-  display: inline-block;
   font-size: calc(16px + 0.5vw);
   font-weight: 700;
-
+  line-height: 125%;
   @media (max-width: 786px) {
     font-size: calc(14px + 0.5vw);
   }
