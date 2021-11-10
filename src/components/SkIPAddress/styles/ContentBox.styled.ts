0b4876b8c6922c6ca2c1a5de7919dc42a5a451cb
shaokiat <shaokiat@gmail.com>
2021-11-10 20:41:20 +0800

    Update Styling

--- src/components/SkIPAddress/styles/ContentBox.styled.ts
+++ src/components/SkIPAddress/styles/ContentBox.styled.ts
@@ -10,7 +10,7 @@ export const StyledContainer = styled.div`
   overflow-y: auto;
   @media (max-width: 786px) {
     width: 100%;
-    margin: 2vh 0 1vh 0;
+    margin: 1.5vh 0 1vh 0;
     justify-content: center;
     align-items: center;
   }
@@ -21,15 +21,17 @@ export const StyledHeader = styled.div`
   font-weight: 700;
   font-size: 12px;
   letter-spacing: 1px;
+  @media (max-width: 786px) {
+    font-size: calc(10px + 0.5vw);
+  }
 `
 
 export const StyledBody = styled.div`
   display: inline-block;
   font-size: calc(16px + 0.5vw);
-  font-weight: 500;
+  font-weight: 700;
 
   @media (max-width: 786px) {
-    font-size: calc(18px + 0.5vw);
+    font-size: calc(14px + 0.5vw);
   }
-  /* word-break: break-all; */
 `
