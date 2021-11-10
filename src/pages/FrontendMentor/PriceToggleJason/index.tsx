1a8925aee66ba8d27145eb291381625adac06b6e
Jason <jason.ng.zq99@gmail.com>
2021-11-10 22:08:50 +0800

    Adds containers to all fields

--- src/pages/FrontendMentor/PriceToggleJason/index.tsx
+++ src/pages/FrontendMentor/PriceToggleJason/index.tsx
@@ -4,26 +4,58 @@ import {
   MainContainer,
   NormalPackageContainerLeft,
   NormalPackageContainerRight,
+  PackageHeader,
+  HighlightPackageHeader,
   PackagesContainer,
   PageHeader,
   ToggleContainer,
   ToggleWord,
+  HighlightAlignmentContainer,
+  NormalPriceContainer,
+  HighlightPriceContainer,
+  NormalDetailsContainer,
+  NormalLearnMoreButton,
+  HighlightDetailsContainer,
+  HighlightLearnMoreButton,
 } from './styles/PriceToggleJasonStyles'
 import { Switch } from 'antd'
 
 export default function PriceToggleJason() {
   return (
     <MainContainer>
-      <PageHeader>Our pricing</PageHeader>
+      <PageHeader>Our Pricing</PageHeader>
       <ToggleContainer>
         <ToggleWord>Annually</ToggleWord>
         <Switch></Switch>
         <ToggleWord>Monthly</ToggleWord>
       </ToggleContainer>
       <PackagesContainer>
-        <NormalPackageContainerLeft></NormalPackageContainerLeft>
-        <HighlightPackageContainer></HighlightPackageContainer>
-        <NormalPackageContainerRight></NormalPackageContainerRight>
+        <NormalPackageContainerLeft>
+          <PackageHeader>Basic</PackageHeader>
+          <NormalPriceContainer>$199.99</NormalPriceContainer>
+          <NormalDetailsContainer>500 GB Storage</NormalDetailsContainer>
+          <NormalDetailsContainer>2 Users Allowed</NormalDetailsContainer>
+          <NormalDetailsContainer>Send up to 3 GB</NormalDetailsContainer>
+          <NormalLearnMoreButton>LEARN MORE</NormalLearnMoreButton>
+        </NormalPackageContainerLeft>
+        <HighlightPackageContainer>
+          <HighlightAlignmentContainer>
+            <HighlightPackageHeader>Professional</HighlightPackageHeader>
+            <HighlightPriceContainer>$249.99</HighlightPriceContainer>
+            <HighlightDetailsContainer>2 TB Storage</HighlightDetailsContainer>
+            <HighlightDetailsContainer>10 Users Allowed</HighlightDetailsContainer>
+            <HighlightDetailsContainer>Send up to 20 GB</HighlightDetailsContainer>
+            <HighlightLearnMoreButton>LEARN MORE</HighlightLearnMoreButton>
+          </HighlightAlignmentContainer>
+        </HighlightPackageContainer>
+        <NormalPackageContainerRight>
+          <PackageHeader>Master</PackageHeader>
+          <NormalPriceContainer>$399.99</NormalPriceContainer>
+          <NormalDetailsContainer>2 TB Storage</NormalDetailsContainer>
+          <NormalDetailsContainer>10 Users Allowed</NormalDetailsContainer>
+          <NormalDetailsContainer>Send up to 20 GB</NormalDetailsContainer>
+          <NormalLearnMoreButton>LEARN MORE</NormalLearnMoreButton>
+        </NormalPackageContainerRight>
       </PackagesContainer>
     </MainContainer>
   )
--- src/pages/FrontendMentor/PriceToggleJason/styles/PriceToggleJasonStyles.tsx
+++ src/pages/FrontendMentor/PriceToggleJason/styles/PriceToggleJasonStyles.tsx
@@ -8,7 +8,7 @@ export const MainContainer = styled.div`
 export const PageHeader = styled.div`
   font-family: 'Montserrat';
   font-weight: 700;
-  font-size: 45px;
+  font-size: 40px;
   text-align: center;
   padding-top: 5vh;
 `
@@ -22,8 +22,9 @@ export const ToggleContainer = styled.div`
 export const ToggleWord = styled.div`
   font-family: 'Montserrat';
   font-size: 20px;
+  color: hsl(204, 8%, 76%);
   line-height: 20px;
-  font-weight: 100;
+  font-weight: 700;
   padding-left: 2vw;
   padding-right: 2vw;
 `
@@ -39,30 +40,122 @@ export const PackagesContainer = styled.div`
 export const NormalPackageContainerLeft = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  margin-top: 3vw;
-  height: 80%;
+  justify-content: center;
+  margin-top: 3vh;
+  height: 90%;
   width: 25%;
   background: hsl(0, 100%, 100%);
   border-radius: 15px 0px 0px 15px;
 `
 
-export const HighlightPackageContainer = styled.div`
+export const NormalPackageContainerRight = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: center;
+  margin-top: 3vh;
+  height: 90%;
+  width: 25%;
+  background: hsl(0, 100%, 100%);
+  border-radius: 0px 15px 15px 0px;
+`
+
+export const PackageHeader = styled.div`
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
+  margin-top: 3vh;
+  color: hsl(233, 13%, 49%);
+`
+
+export const NormalPriceContainer = styled.div`
+  margin: auto;
+  font-size: 50px;
+  font-weight: 800;
+  text-align: center;
+  width: 80%;
+  margin-top: 2vh;
+  height: 12vh;
+  border-bottom: 1px solid grey;
+`
+
+export const NormalDetailsContainer = styled.div`
+  text-align: center;
+  margin: auto;
+  font-size: 16px;
+  padding-bottom: 2vh;
+  font-weight: 800;
+  width: 80%;
+  border-bottom: 1px solid grey;
+  color: hsl(233, 13%, 49%);
+`
+
+export const NormalLearnMoreButton = styled.button`
+  border-radius: 5px;
+  background: linear-gradient(hsl(236, 72%, 79%), hsl(237, 63%, 64%));
+  color: hsl(0, 100%, 100%);
+  height: 5vh;
+  width: 80%;
+  margin: auto;
+  font-weight: 800;
+  border: none;
+`
+
+export const HighlightPackageContainer = styled.div`
   width: 25%;
   background: linear-gradient(hsl(236, 72%, 79%), hsl(237, 63%, 64%));
   border-radius: 15px;
 `
 
-export const NormalPackageContainerRight = styled.div`
+export const HighlightPackageHeader = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
-  margin-top: 3vw;
-  height: 80%;
-  width: 25%;
+  justify-content: center;
+  margin-top: 3vh;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 800;
+  color: hsl(0, 100%, 100%);
+`
+
+export const HighlightAlignmentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 90%;
+  margin-top: 3vh;
+`
+
+export const HighlightPriceContainer = styled.div`
+  margin: auto;
+  margin-top: 2vh;
+  height: 12vh;
+  font-size: 50px;
+  font-weight: 800;
+  width: 80%;
+  color: hsl(0, 100%, 100%);
+  text-align: center;
+  border-bottom: 1px solid grey;
+`
+
+export const HighlightDetailsContainer = styled.div`
+  text-align: center;
+  margin: auto;
+  font-size: 16px;
+  color: hsl(0, 100%, 100%);
+  padding-bottom: 2vh;
+  font-weight: 800;
+  width: 80%;
+  vertical-align: middle;
+  border-bottom: 1px solid grey;
+`
+
+export const HighlightLearnMoreButton = styled.button`
+  border-radius: 5px;
   background: hsl(0, 100%, 100%);
-  border-radius: 0px 15px 15px 0px;
+  color: hsl(237, 63%, 64%);
+  height: 5vh;
+  width: 80%;
+  margin: auto;
+  font-weight: 800;
+  border: none;
 `
