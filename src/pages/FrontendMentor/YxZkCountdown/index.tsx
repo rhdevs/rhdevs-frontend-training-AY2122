fa9f5fb8b9c48a0f4c2dac1431cce2eea8c3735d
Zhang Zhikai <zhangzhikai71@gmail.com>
2021-11-10 21:46:53 +0800

    Improve responsiveness of the background

--- src/pages/FrontendMentor/YxZkCountdown/index.tsx
+++ src/pages/FrontendMentor/YxZkCountdown/index.tsx
@@ -2,17 +2,26 @@ import React from 'react'
 
 import BackgroundStars from '../../../assets/YxZkCountdown/bg-stars.svg'
 import BackgroundHills from '../../../assets/YxZkCountdown/pattern-hills.svg'
-import { BackgroundContainer, Background, Header } from './styles/YxZkCountdown.styled'
+import {
+  BackgroundContainer,
+  StarBackground,
+  HillBackground,
+  Header,
+  StarBackgroundContainer,
+  HillBackgroundContainer,
+} from './styles/YxZkCountdown.styled'
 
 const CountdownTimer = () => {
   return (
-    <>
-      <BackgroundContainer>
-        <Header>Hello</Header>
-        <Background src={BackgroundStars} />
-        <Background src={BackgroundHills} />
-      </BackgroundContainer>
-    </>
+    <BackgroundContainer>
+      <Header>Hello</Header>
+      <StarBackgroundContainer>
+        <StarBackground src={BackgroundStars} />
+      </StarBackgroundContainer>
+      <HillBackgroundContainer>
+        <HillBackground src={BackgroundHills} />
+      </HillBackgroundContainer>
+    </BackgroundContainer>
   )
 }
 
--- src/pages/FrontendMentor/YxZkCountdown/styles/YxZkCountdown.styled.ts
+++ src/pages/FrontendMentor/YxZkCountdown/styles/YxZkCountdown.styled.ts
@@ -4,17 +4,44 @@ export const BackgroundContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-color: hsl(235, 16%, 14%);
+  height: 100vh;
+  background-color: #1e1f29;
   background-size: cover;
+  background-repeat: no-repeat;
   position: relative;
+  overflow: hidden;
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `
-export const Background = styled.img`
-  width: 100%;
+
+export const StarBackgroundContainer = styled.div`
+  display: flex;
   position: relative;
+  height: 80vh;
 `
+export const StarBackground = styled.img`
+  width: 100vw;
+  display: flex;
+  position: relative;
+  background-position: top;
+`
+export const HillBackgroundContainer = styled.div`
+  background-size: cover;
+  height: 20vh;
+`
+export const HillBackground = styled.img`
+  width: 100vw;
+  height: 100%;
+  margin-top: auto;
+  margin-bottom: auto;
+  @media (max-width: 768px) {
+  }
+`
+
 export const Header = styled.div`
   width: 100%;
-  font-size: 2.5rem;
+  font-size: 4.5vw;
   font-weight: 500;
   position: absolute;
   color: white;
