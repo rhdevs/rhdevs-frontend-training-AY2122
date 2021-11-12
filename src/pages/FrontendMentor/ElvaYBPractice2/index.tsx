7d2e807dc8d9c670d5b56a4439e40906eaab35bc
Wang Yongbin <yongbin_01@hotmail.com>
2021-11-13 00:42:07 +0800

    Included all components, left with styling

--- src/pages/FrontendMentor/ElvaYBPractice2/index.tsx
+++ src/pages/FrontendMentor/ElvaYBPractice2/index.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react'
 import { Slider, Switch } from 'antd'
 
+import IconCheck from '../../../assets/ElvaYongbin/images/icon-check.svg'
+
 import {
   MainPage,
   TopComponent,
   TopComponentTopText,
   TopComponentBottomText,
   MainComponent,
-  SliderBar,
   Slidercontainer,
   PageViewPriceContainer,
   PageViewContainer,
   PriceContainer,
+  MonthContainer,
+  StartTrialButton,
+  BottomLeftContainer,
+  BottomLeftText,
+  MainBottomComponent,
+  ToggleContainer,
+  DiscountButton,
+  CheckIcon,
+  ToggleContainerText,
+  TrialButtonContainer,
 } from './styles/SliderComponent.styled'
 
 const marks = {
@@ -37,26 +48,26 @@ const ElvaYBPractice = () => {
           <PageViewPriceContainer>
             {toggle === false ? (
               views === 0 ? (
-                <PageViewContainer>{10}k Pageviews</PageViewContainer>
+                <PageViewContainer>{10}k PAGEVIEWS</PageViewContainer>
               ) : views === 25 ? (
-                <PageViewContainer>{50}k Pageviews</PageViewContainer>
+                <PageViewContainer>{50}k PAGEVIEWS</PageViewContainer>
               ) : views === 50 ? (
-                <PageViewContainer>{100}k Pageviews</PageViewContainer>
+                <PageViewContainer>{100}k PAGEVIEWS</PageViewContainer>
               ) : views === 75 ? (
-                <PageViewContainer>{500}k Pageviews</PageViewContainer>
+                <PageViewContainer>{500}k PAGEVIEWS</PageViewContainer>
               ) : (
-                <PageViewContainer>{1}M Pageviews</PageViewContainer>
+                <PageViewContainer>{1}M PAGEVIEWS</PageViewContainer>
               )
             ) : views === 0 ? (
-              <PageViewContainer>{10}k Pageviews</PageViewContainer>
+              <PageViewContainer>{10}k PAGEVIEWS</PageViewContainer>
             ) : views === 25 ? (
-              <PageViewContainer>{50}k Pageviews</PageViewContainer>
+              <PageViewContainer>{50}k PAGEVIEWS</PageViewContainer>
             ) : views === 50 ? (
-              <PageViewContainer>{100}k Pageviews</PageViewContainer>
+              <PageViewContainer>{100}k PAGEVIEWS</PageViewContainer>
             ) : views === 75 ? (
-              <PageViewContainer>{500}k Pageviews</PageViewContainer>
+              <PageViewContainer>{500}k PAGEVIEWS</PageViewContainer>
             ) : (
-              <PageViewContainer>{1}M Pageviews</PageViewContainer>
+              <PageViewContainer>{1}M PAGEVIEWS</PageViewContainer>
             )}
 
             {toggle === false ? (
@@ -82,12 +93,33 @@ const ElvaYBPractice = () => {
             ) : (
               <PriceContainer>$27.00</PriceContainer>
             )}
-            <PriceContainer>/Month</PriceContainer>
+            <MonthContainer>/Month</MonthContainer>
           </PageViewPriceContainer>
           <Slidercontainer>
             <Slider marks={marks} onChange={(value) => setViews(value)} step={null} defaultValue={50} />
           </Slidercontainer>
-          <Switch onChange={(checked) => setToggle(checked)} />
+          <ToggleContainer>
+            <ToggleContainerText>Monthly Billing</ToggleContainerText>
+            <Switch onChange={(checked) => setToggle(checked)} />
+            <ToggleContainerText>Yearly Billing</ToggleContainerText>
+            <DiscountButton>25% discount</DiscountButton>
+          </ToggleContainer>
+          <MainBottomComponent>
+            <BottomLeftContainer>
+              <BottomLeftText>
+                <CheckIcon src={IconCheck} alt="Tick"></CheckIcon>Unlimited websites
+              </BottomLeftText>
+              <BottomLeftText>
+                <CheckIcon src={IconCheck} alt="Tick"></CheckIcon>100% data ownership
+              </BottomLeftText>
+              <BottomLeftText>
+                <CheckIcon src={IconCheck} alt="Tick"></CheckIcon>Email reports
+              </BottomLeftText>
+            </BottomLeftContainer>
+            <TrialButtonContainer>
+              <StartTrialButton>Start my trial</StartTrialButton>
+            </TrialButtonContainer>
+          </MainBottomComponent>
         </MainComponent>
       </MainPage>
     </>
--- src/pages/FrontendMentor/ElvaYBPractice2/styles/SliderComponent.styled.ts
+++ src/pages/FrontendMentor/ElvaYBPractice2/styles/SliderComponent.styled.ts
@@ -14,72 +14,138 @@ export const MainPage = styled.div`
   justify-content: center;
   background-image: url(${BackgroundPic});
   background-repeat: no-repeat;
-  background-size: 100% 50%;
+  background-size: 100% 100%;
+  background-position: 0vw -40vh;
   color: hsl(230, 100%, 99%);
 `
 
 export const TopComponent = styled.div`
   background-image: url(${PatternCirclePic});
-  background-size: 100% 100%;
+  background-size: 40% 40%;
   background-repeat: no-repeat;
-  padding: 10px;
+  background-position: center;
   width: 40%;
+  height: 20vh;
   text-align: center;
-  position: relative;
+  position: absolute;
+  top: 50px;
+  align-items: center;
 `
 export const TopComponentTopText = styled.div`
   color: black;
-  font-size: 15px;
+  font-size: 20px;
+  height: 10vh;
+  align-items: center;
 `
 export const TopComponentBottomText = styled.div`
   color: grey;
-  font-size: 7px;
+  font-size: 15px;
+  height: 10vh;
 `
 export const MainComponent = styled.div`
   z-index: 2;
   border-radius: 15px;
-  width: 70vw;
-  height: 70vh;
+  width: 33vw;
+  height: 50vh;
   display: flex;
   position: relative;
-  background-color: white;
+  background-color: #ffffff;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  box-shadow: 5px 5px black;
+  top: -30px;
 `
 
-export const SliderBoxComponent = styled.div``
+export const ToggleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding-left: 100px;
+`
 export const Slidercontainer = styled.div`
   width: 60%;
   align-items: center;
+  display: flex;
+  flex-direction: row;
 `
 
-export const SliderBar = styled.div`
-  webkit-appearance: none;
-  appearance: none;
+export const MainBottomComponent = styled.div`
   width: 100%;
-  height: 25px;
-  background: #d3d3d3;
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: 0.2s;
-  transition: opacity 0.2s;
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 -1px 0 #d3d3d3;
 `
-export const MainBottomComponent = styled.div``
 export const PageViewPriceContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
+  margin-top: -20vh;
 `
 export const PageViewContainer = styled.div`
   width: 50%;
-  color: black;
+  color: hsl(225, 20%, 60%);
   text-align: center;
+  margin-left: 20px;
+  font-size: 15px;
 `
 export const PriceContainer = styled.div`
+  width: 10%;
+  text-align: right;
+  margin-left: 40px;
+  font-weight: 600;
+  color: hsl(227, 35%, 25%);
+  font-size: 50px;
+`
+export const MonthContainer = styled.div`
   width: 50%;
-  color: black;
+  text-align: center;
+  color: hsl(225, 20%, 60%);
+`
+export const StartTrialButton = styled.button`
+  background-color: hsl(227, 35%, 25%);
+  border-radius: 20px;
+  width: 65%;
+  height: 35%;
+  color: hsl(226, 100%, 87%);
+`
+export const BottomLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  height: 100%;
+  align-items: center;
+  padding-right: 30px;
+  padding-top: 20px;
+`
+export const BottomLeftText = styled.div`
+  color: hsl(225, 20%, 60%);
+  margin-top: 10px;
+`
+export const TrialButtonContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  align-items: center;
+  display: flex;
+  margin-top: 30px;
+`
+export const DiscountButton = styled.button`
+  background-color: hsl(14, 92%, 95%);
+  color: hsl(15, 100%, 70%);
+  border-radius: 15px;
+  font-size: 10px;
+  width: 55%;
+  height: 50%;
+  margin-left: 20px;
+  border: none;
+`
+export const CheckIcon = styled.img`
+  object-fit: scale-down;
+  vertical-align: middle;
+  margin-left: 1vh;
+`
+export const ToggleContainerText = styled.div`
+  color: hsl(225, 20%, 60%);
+  width: 70%;
+  height: 100%;
   text-align: center;
 `
-export const RightTrialButton = styled.div``
-export const LeftTrialText = styled.div``
