86996b35a71e3f82d386cd9ca454aab355ec108b
Jia Yixuan <jyxhazcake@yahoo.com>
2021-12-03 00:19:59 +0800

    Add TimeBoxCard component

--- src/components/YxZkCountdown/TimeCard.tsx
+++ src/components/YxZkCountdown/TimeCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-
-import { Countdown, TimeBox, TimeCardContainer, TimeCardLabel } from './styles/TimeCard.styled'
+import TimeBoxCard from './TimeBoxCard'
+import { TimeBox, TimeCardContainer, TimeCardLabel } from './styles/TimeCard.styled'
 type Props = {
   days: number
   hours: number
@@ -11,42 +11,10 @@ type Props = {
 const TimeCard = (props: Props) => {
   return (
     <TimeCardContainer>
-      <TimeBox>
-        <TimeBox className="tophalf" />
-        <TimeBox className="bottomhalf" />
-        <TimeBox className="bar" />
-        <TimeBox className="number">{props.days}</TimeBox>
-        <TimeBox className="right-curve" />
-        <TimeBox className="left-curve" />
-        <TimeCardLabel>Days</TimeCardLabel>
-      </TimeBox>
-      <TimeBox>
-        <TimeBox className="tophalf" />
-        <TimeBox className="bottomhalf" />
-        <TimeBox className="bar" />
-        <TimeBox className="number">{props.hours}</TimeBox>
-        <TimeBox className="right-curve" />
-        <TimeBox className="left-curve" />
-        <TimeCardLabel>Hours</TimeCardLabel>
-      </TimeBox>
-      <TimeBox>
-        <TimeBox className="tophalf" />
-        <TimeBox className="bottomhalf" />
-        <TimeBox className="bar" />
-        <TimeBox className="number">{props.minutes}</TimeBox>
-        <TimeBox className="right-curve" />
-        <TimeBox className="left-curve" />
-        <TimeCardLabel>Minutes</TimeCardLabel>
-      </TimeBox>
-      <TimeBox>
-        <TimeBox className="tophalf" />
-        <TimeBox className="bottomhalf" />
-        <TimeBox className="bar" />
-        <TimeBox className="number">{props.seconds}</TimeBox>
-        <TimeBox className="right-curve" />
-        <TimeBox className="left-curve" />
-        <TimeCardLabel>Seconds</TimeCardLabel>
-      </TimeBox>
+      <TimeBoxCard value={props.days} label="days" />
+      <TimeBoxCard value={props.hours} label="hours" />
+      <TimeBoxCard value={props.minutes} label="minutes" />
+      <TimeBoxCard value={props.seconds} label="seconds" />
     </TimeCardContainer>
   )
 }
