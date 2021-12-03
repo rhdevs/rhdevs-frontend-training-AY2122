2a5dd4884218f21c290c3e6d55a2138e8924a8d4
Maxim Yam <maximyam@gmail.com>
2021-12-03 23:17:13 +0800

    rectified indexhtml and wrapper

--- src/pages/FrontendMentor/Maximwp_proj2/index.tsx
+++ src/pages/FrontendMentor/Maximwp_proj2/index.tsx
@@ -16,40 +16,38 @@ export default function PricingApp() {
   const [monthly, setMonthly] = useState(false)
 
   return (
-    <>
-      <MainBox>
-        <TopBox>
-          <Pricing> Our Pricing </Pricing>
-          <Minibox>
-            <ToggleBetween> Annually </ToggleBetween>
-            <SwitchStyle1 onChange={() => setMonthly(!monthly)}></SwitchStyle1>
-            <ToggleBetween> Monthly </ToggleBetween>
-          </Minibox>
-        </TopBox>
-        <MainContainer>
-          <Container
-            title="Basic"
-            number={monthly ? '$19.99' : '$199.99'}
-            text1="500 GB Storage"
-            text2={'2 Users Allowed'}
-            text3={'Send up to 3 GB'}
-          ></Container>
-          <CentCont
-            title="Professional"
-            number={monthly ? '$24.99' : '$249.99'}
-            text1="1 TB Storage"
-            text2={'5 Users Allowed'}
-            text3={'Send up to 10 GB'}
-          ></CentCont>
-          <Container
-            title="Master"
-            number={monthly ? '$39.99' : '$359.99'}
-            text1={'2 TB Storage'}
-            text2={'10 Users Allowed'}
-            text3={'Send up to 20 GB'}
-          ></Container>
-        </MainContainer>
-      </MainBox>
-    </>
+    <MainBox>
+      <TopBox>
+        <Pricing> Our Pricing </Pricing>
+        <Minibox>
+          <ToggleBetween> Annually </ToggleBetween>
+          <SwitchStyle1 onChange={() => setMonthly(!monthly)}></SwitchStyle1>
+          <ToggleBetween> Monthly </ToggleBetween>
+        </Minibox>
+      </TopBox>
+      <MainContainer>
+        <Container
+          title="Basic"
+          number={monthly ? '$19.99' : '$199.99'}
+          text1="500 GB Storage"
+          text2={'2 Users Allowed'}
+          text3={'Send up to 3 GB'}
+        ></Container>
+        <CentCont
+          title="Professional"
+          number={monthly ? '$24.99' : '$249.99'}
+          text1="1 TB Storage"
+          text2={'5 Users Allowed'}
+          text3={'Send up to 10 GB'}
+        ></CentCont>
+        <Container
+          title="Master"
+          number={monthly ? '$39.99' : '$359.99'}
+          text1={'2 TB Storage'}
+          text2={'10 Users Allowed'}
+          text3={'Send up to 20 GB'}
+        ></Container>
+      </MainContainer>
+    </MainBox>
   )
 }
